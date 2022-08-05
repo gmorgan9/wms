2f842c5149bb4c5bef90824b6ba7f3151376391a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-05 13:39:20 -0600

    update

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -1,6 +1,6 @@
 <!-- WORKING -->
 <footer class="fixed-bottom py-3">
-<?php if(isset($_SESSION['user_fname']) || isset($_SESSION['admin_fname'])){ ?>
+<?php if(!isset($_SESSION['user_fname']) || !isset($_SESSION['admin_fname'])){ ?>
     <div class="text-center text-muted" style="background-color: rgba(0, 0, 0, 0.05);">
 <?php } else { ?>
     <div class="text-center text-muted" style="background-color: white;">
