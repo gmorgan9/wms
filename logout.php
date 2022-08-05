94c1625609aa6aeaaa246551daa8bc69fe2bde83
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-05 11:46:58 -0600

    update

---
+++ logout.php
@@ -0,0 +1,19 @@
+<!-- WORKING -->
+<?php
+session_start();
+require_once "app/database/connection.php";
+require_once "path.php";
+
+$empID = $_SESSION['empID'];
+$sql = "UPDATE employees SET loggedin='0' WHERE employeeID='$empID'";
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+session_unset();
+session_destroy();
+header('location:' . BASE_URL . '/admin/profile.php');
+
+?>
