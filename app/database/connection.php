9b2e3a20aee2f5cf8c5ff5d21284d26588cebfe4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-05 10:49:50 -0600

    update

---
+++ app/database/connection.php
@@ -0,0 +1,16 @@
+<!-- WORKING -->
+<?php
+
+$servername = "localhost";
+$username = "garrett";
+$password = "BIGmorgan1999!";
+$database = "ets";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+?>
---
+++ app/includes/footer.php
@@ -0,0 +1,6 @@
+<!-- WORKING -->
+<footer class="fixed-bottom py-3">
+    <div class="text-center text-muted" style="background-color: rgba(0, 0, 0, 0.05);">
+        &copy; 2022 Employee Timesheet System
+    </div>
+</footer>
