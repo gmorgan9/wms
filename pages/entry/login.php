507f52f4d8cea0100c3551e49ba6cc8923c83583
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-05 10:57:22 -0600

    update

---
+++ pages/entry/login.php
@@ -0,0 +1,110 @@
+<!-- WORKING -->
+<?php
+
+require_once "app/database/connection.php";
+// require_once "app/database/functions.php";
+require_once "../../path.php";
+
+session_start();
+
+if(isLoggedIn()){
+   header('location: /dashboard.php');
+}
+
+
+
+if(isset($_POST['submit'])){
+
+   $empID = mysqli_real_escape_string($conn, $_POST['employeeID']);
+   $fname = mysqli_real_escape_string($conn, $_POST['fname']);
+   $lname = mysqli_real_escape_string($conn, $_POST['lname']);
+   $uname = mysqli_real_escape_string($conn, $_POST['uname']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = md5($_POST['password']);
+   $cpass = md5($_POST['cpassword']);
+   $isadmin = $_POST['isadmin'];
+   $loggedin = $_POST['loggedin'];
+
+   $select = " SELECT * FROM employees WHERE uname = '$uname' && password = '$pass' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+
+      $row = mysqli_fetch_array($result);
+      $sql = "UPDATE employees SET loggedin='1' WHERE uname='$uname'";
+      if($row['isadmin'] == 1){
+         if (mysqli_query($conn, $sql)) {
+            echo "Record updated successfully";
+          } else {
+            echo "Error updating record: " . mysqli_error($conn);
+          }
+         
+         $_SESSION['sID'] = $row['studentID'];
+         $_SESSION['loggedin'] = $row['loggedin'];
+         $_SESSION['isadmin'] = $row['isadmin'];
+         header('location: /admin/profile.php');
+      }elseif($row['isadmin'] == 0){
+         if (mysqli_query($conn, $sql)) {
+            echo "Record updated successfully";
+          } else {
+            echo "Error updating record: " . mysqli_error($conn);
+          }
+          $_SESSION['sID'] = $row['studentID'];
+          $_SESSION['loggedin'] = $row['loggedin'];
+          $_SESSION['isadmin'] = $row['isadmin'];
+         header('location: profile.php');
+      }
+     
+   }else{
+      $error[] = 'incorrect email or password!';
+   }
+
+};
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>ETS | Login</title>
+
+<!-- Custom Styles -->
+<link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/main-style.css?v='. time(); ?>">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+<!-- Bootstrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+</head>
+<body>
+
+<?php include("app/includes/header.php"); ?>
+   
+<br><br><br>
+<div class="form-container mx-auto">
+
+   <form action="" method="post">
+      <h3>login now</h3>
+      <?php
+      if(isset($error)){
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+         };
+      };
+      ?>
+      <input type="text" name="uname" required placeholder="enter your user name">
+      <input type="password" name="password" required placeholder="enter your password">
+      <input type="submit" name="submit" value="Login" class="form-btn">
+      <p>don't have an account? <a href="register.php">register now</a></p>
+   </form>
+
+</div>
+
+<?php include("app/includes/footer.php"); ?>
+
+</body>
+</html>
---
+++ pages/entry/register.php
@@ -0,0 +1,102 @@
+<!-- WORKING -->
+<?php
+
+require_once "app/database/connection.php";
+// require_once "app/database/functions.php";
+require_once "../../path.php";
+
+session_start();
+
+// if(isLoggedIn()){
+//    header('location: /dashboard.php');
+// }
+
+
+if(isset($_POST['submit'])){
+
+   $empID = mysqli_real_escape_string($conn, $_POST['employeeID']);
+   $fname = mysqli_real_escape_string($conn, $_POST['fname']);
+   $lname = mysqli_real_escape_string($conn, $_POST['lname']);
+   $uname = mysqli_real_escape_string($conn, $_POST['uname']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = md5($_POST['password']);
+   $cpass = md5($_POST['cpassword']);
+   $isadmin = $_POST['isadmin'];
+
+   $select = " SELECT * FROM employees WHERE uname = '$uname' && email = '$email' && password = '$pass' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+
+      $error[] = 'user already exist!';
+
+   }else{
+
+      if($pass != $cpass){
+         $error[] = 'passwords do not match!';
+      }else{
+         $insert = "INSERT INTO employees (fname, lname, uname, email, password) VALUES('$fname','$lname','$uname','$email','$pass')";
+         mysqli_query($conn, $insert);
+         header('location:login.php');
+      }
+   }
+
+};
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>CAMS | Registration</title>
+
+   <!-- Custom Styles -->
+<link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/main-style.css?v='. time(); ?>">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+<!-- Bootstrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+</head>
+<body>
+<?php include("app/includes/header.php"); ?>
+   
+<br><br><br>
+<div class="form-container mx-auto">
+
+
+   <form action="" method="post">
+      <h3>register now</h3>
+      <?php
+      if(isset($error)){
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+         };
+      };
+      ?>
+      <input type="text" name="fname" required placeholder="enter your first name">
+      <input type="text" name="lname" required placeholder="enter your last name">
+      <input type="text" name="uname" required placeholder="enter your user name">
+      <input type="email" name="email" required placeholder="enter your email">
+      <input type="password" name="password" required placeholder="enter your password">
+      <input type="password" name="cpassword" required placeholder="confirm your password">
+      <!-- <select name="user_type">
+         <option value="user">user</option>
+         <option value="admin">admin</option>
+      </select> -->
+      <input type="submit" name="submit" value="register now" class="form-btn">
+      <p>already have an account? <a href="student-login.php">login now</a></p>
+   </form>
+
+</div>
+
+<?php include("app/includes/footer.php"); ?>
+
+</body>
+</html>
