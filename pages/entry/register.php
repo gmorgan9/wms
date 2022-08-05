6f6ad07e04f4776f1e7fe9ca8ed66fd96667a389
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-05 11:51:57 -0600

    update

--- pages/entry/register.php
+++ pages/entry/register.php
@@ -2,14 +2,14 @@
 <?php
 
 require_once "../../app/database/connection.php";
-// require_once "app/database/functions.php";
+require_once "../../app/database/functions.php";
 require_once "../../path.php";
 
 session_start();
 
-// if(isLoggedIn()){
-//    header('location: /dashboard.php');
-// }
+if(isLoggedIn()){
+   header('location: /dashboard.php');
+}
 
 
 if(isset($_POST['submit'])){
