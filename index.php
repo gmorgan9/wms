132b6e626cf50cde49530d06d2b72a9b9188c39e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-05 10:38:02 -0600

    update

--- index.php
+++ index.php
@@ -20,7 +20,8 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!-- Custom Styles -->
-   <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/main-style.css?v='. time(); ?>">
+   <!-- <link rel="stylesheet" href="<?php //echo BASE_URL . '/assets/css/main-style.css?v='. time(); ?>"> -->
+   <link rel="stylesheet" href="/assets/css/main-style.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
