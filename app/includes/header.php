c76177637c148c93796e752261dc40c5cd5d7689
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-05 10:07:35 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -12,7 +12,7 @@
 
 <!-- Brand -->
         <a class="navbar-brand" href="/">
-            <h3><i class="bi bi-file-earmark-check"></i> Course Assignment Management System</h3>
+            <h3><i class="bi bi-file-earmark-check"></i> Employee Timesheets System</h3>
         </a>
 
         <ul class="navbar-nav ms-auto d-flex flex-row">
