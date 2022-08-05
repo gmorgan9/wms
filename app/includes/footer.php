ba0972f19faacfa41d3c50a12891a2d94d8480cd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-05 13:41:44 -0600

    update

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -1,9 +1,10 @@
 <!-- WORKING -->
-<footer class="fixed-bottom py-3">
 <?php if(isset($_SESSION['']) || isset($_SESSION['admin_fname'])){ ?>
-    <div class="text-center text-muted" style="background-color: white;">
+    <footer class="fixed-bottom py-3" style="background-color: white">
+        <div class="text-center text-muted" style="background-color: white;">
 <?php } else { ?>
-    <div class="text-center text-muted" style="background-color: rgba(0, 0, 0, 0.05);">
+    <footer class="fixed-bottom py-3" style="background-color: #17181C;">
+        <div class="text-center text-muted" style="background-color: rgba(0, 0, 0, 0.05);">
 <?php }?>
         &copy; 2022 Employee Timesheet System
     </div>
