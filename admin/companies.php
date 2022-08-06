d0a0c1d04e055f092170c2c8cfaab4d21e0b382f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-05 23:49:00 -0600

    update

--- admin/companies.php
+++ admin/companies.php
@@ -112,10 +112,10 @@ if (mysqli_num_rows($result) > 0) {
 ?>
 
   <div class="page-header mx-auto">
-    <p class="page_title" style="float: left; padding-top: 2px;">Company's</p>
+    <p class="page_title" style="float: left; padding-top: 2px;">Companies</p>
     <ul class="breadcrumb">
       <li><a href="<?php echo BASE_URL . '/pages/dashboard.php' ?>">Dashboard</a></li>
-      <li>Company's</li>
+      <li>Companies</li>
     </ul>
   </div>
 
--- admin/users.php
+++ admin/users.php
@@ -72,7 +72,7 @@ if(isset($_GET['employeeID'])) {
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>ETES | Manage Users</title>
+   <title>WMS | Users</title>
 
    <!-- Custom Styles -->
    <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/other-style.css?v='. time(); ?>">
@@ -112,10 +112,10 @@ if (mysqli_num_rows($result) > 0) {
 ?>
 
   <div class="page-header mx-auto">
-    <p class="page_title" style="float: left; padding-top: 2px;">Manage Users</p>
+    <p class="page_title" style="float: left; padding-top: 2px;">Users</p>
     <ul class="breadcrumb">
       <li><a href="<?php echo BASE_URL . '/pages/dashboard.php' ?>">Dashboard</a></li>
-      <li>Manage Users</li>
+      <li>Users</li>
     </ul>
   </div>
 
