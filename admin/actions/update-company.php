743d85d768e332b0341b3f074e8c2478a2d1fdac
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-06 09:26:11 -0600

    update

--- admin/actions/update-company.php
+++ admin/actions/update-company.php
@@ -1,9 +1,9 @@
 <!-- WORKING -->
 <?php
 
-require_once "../app/database/connection.php";
-require_once "../app/database/functions.php";
-require_once "../path.php";
+require_once "../../app/database/connection.php";
+require_once "../../app/database/functions.php";
+require_once "../../path.php";
 
 session_start();
 
@@ -15,40 +15,34 @@ if(!isAdmin()){
 }
 
 
-// $empID = $_SESSION['empID'];
-
+// $compID = $_SESSION['empID'];
+// $select = " SELECT * FROM employee WHERE employeeID = '$empID' ";
+// $result = mysqli_query($conn, $select);
 
 if(isset($_POST['update-company'])){
 
-   $companyID   = mysqli_real_escape_string($conn, $_POST['companyID']);
-   $companyname = mysqli_real_escape_string($conn, $_POST['companyname']);
-   $ccity = mysqli_real_escape_string($conn, $_POST['ccity']);
-   $cstate = mysqli_real_escape_string($conn, $_POST['cstate']);
-   $czip = mysqli_real_escape_string($conn, $_POST['czip']);
-   // $pass = md5($_POST['password']);
-   // $cpass = md5($_POST['cpassword']);
-   // $isadmin = $_POST['isadmin'];
-
-   $update_select = " SELECT * FROM company WHERE companyID = '$companyID' ";
-
-   $update_result = mysqli_query($conn, $update_select);
-
-   if(mysqli_num_rows($update_result) > 0){
-
-      // $error[] = 'user already exist!';
-      $update = "UPDATE company SET companyname = '$companyname', ccity = '$ccity', cstate = '$cstate', czip = '$czip' where companyID = '$companyID' ";
-      mysqli_query($conn, $update);
-      $success[] = 'Success';
-      //header('location:' . BASE_URL . '/admin/profile.php');
-      
-   }else{
-      
-   } 
-};
-
-// $select = " SELECT * FROM company WHERE companyID = '$companyID' ";
-$result = mysqli_query($conn, $update_select);
-
+    $compID   = mysqli_real_escape_string($conn, $_POST['companyID']);
+    $cname = mysqli_real_escape_string($conn, $_POST['companyname']);
+    $ccity = mysqli_real_escape_string($conn, $_POST['ccity']);
+    $cstate = mysqli_real_escape_string($conn, $_POST['cstate']);
+    $czip = mysqli_real_escape_string($conn, $_POST['czip']);
+ 
+    $select = " SELECT * FROM company WHERE companyID = '$compID'";
+    $update_result = mysqli_query($conn, $select);
+ 
+    if(mysqli_num_rows($update_result) > 0){
+ 
+       // $error[] = 'user already exist!';
+       $update = "UPDATE company SET companyname = '$cname', ccity = '$ccity', cstate = '$cstate', czip = '$czip' where companyID = '$compID' ";
+       mysqli_query($conn, $update);
+       $success[] = 'Success';
+       //header('location:' . BASE_URL . '/admin/companies.php');
+    //    header('Location: ' . $_SERVER['HTTP_REFERER']);
+       
+    }else{
+       $error[] = "Error";
+    } 
+ };
 ?>
 
 <!DOCTYPE html>
@@ -57,7 +51,7 @@ $result = mysqli_query($conn, $update_select);
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>WMS | Admin Profile</title>
+   <title>WMS | Update Company</title>
 
    <!-- Custom Styles -->
    <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/other-style.css?v='. time(); ?>">
@@ -92,34 +86,35 @@ $result = mysqli_query($conn, $update_select);
    
 <?php 
 
- if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
+// if (mysqli_num_rows($result) > 0) {
+//    while($row = mysqli_fetch_assoc($result)) {
 //       $acc_type = $row['acc_type'];
- ?>
+?>
 
   <div class="page-header mx-auto">
-    <p class="page_title" style="float: left; padding-top: 2px;">Profile</p>
+    <p class="page_title" style="float: left; padding-top: 2px;">Add Company</p>
     <ul class="breadcrumb">
       <li><a href="<?php echo BASE_URL . '/pages/dashboard.php' ?>">Dashboard</a></li>
-      <li>Admin Profile</li>
+      <li><a href="<?php echo BASE_URL . '/admin/companies.php' ?>">Companies</a></li>
+      <li>Add Company</li>
     </ul>
   </div>
 
-  <div class="jumbotron jumbotron-fluid bg-white m-2 mx-auto" style="width: 98%;">
+  <!-- <div class="jumbotron jumbotron-fluid bg-white m-2 mx-auto" style="width: 98%;">
   <div class="container">
-    <h3 class="display-6 text-center" style="padding-top: 5px !important;padding-bottom: 10px !important;">Welcome, <span style="text-transform: capitalize;"><?php echo $row['fname'] ?>!</span></h3>
+    <h3 class="display-6 text-center" style="padding-top: 5px !important;padding-bottom: 10px !important;">Welcome, <span style="text-transform: capitalize;"><?php //echo $row['fname'] ?>!</span></h3>
   </div>
-</div>
+</div> -->
 
-<div class="page-content mx-auto">
+<div class="page-content mx-auto mt-2">
 <form action="" method="post">
-      <h3 class="mx-auto" style="width: 95%;">Admin Profile</h3>
+      <h3 class="mx-auto" style="width: 95%;">Update Company Data</h3>
       <?php
-      // if(isset($error)){
-      //    foreach($error as $error){
-      //       echo '<span class="error-msg">'.$error.'</span>';
-      //    };
-      // };
+      if(isset($error)){
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+         };
+      };
 
       if(isset($success)){
          foreach($success as $success){
@@ -127,52 +122,44 @@ $result = mysqli_query($conn, $update_select);
          };
       };
       ?> 
-      <div class="row" style="margin-left: 20px;">
-      <div class="form-group pt-3" style="width: 20%;">
+      <?php
+      $sql = "SELECT * FROM company";
+      $all = mysqli_query($conn, $sql);
+      if($all) {
+          while ($row = mysqli_fetch_assoc($all)) {
+            $compID   = $row['companyID'];
+            $cname    = $row['companyname'];
+            $ccity    = $row['ccity'];
+            $cstate   = $row['cstate'];
+            $czip     = $row['czip'];
+  ?>
+      <div class="form-group pt-3 mx-auto" style="width: 95%;">
             <label for="companyID">Company ID</label>
-            <input class="form-control" style="width: 90%" id="companyID" type="text" value="<?php echo $row['companyID']; ?>" name="companyID" disabled>
+            <input class="form-control" style="width: 20%" id="companyID" type="text" value="<?php echo $compID; ?>" name="companyID" disabled>
          </div>
-
-      <!-- <div class="row" style="margin-left: 20px;"> -->
-         <div class="form-group pt-3 mx-auto" style="width: 95%;">
+      <div class="form-group pt-3 mx-auto" style="width: 95%;">
             <label for="companyname">Company Name</label>
-            <input class="form-control" id="companyname" type="text" name="companyname" value="<?php echo $row['companyname']; ?>" required>
+            <input class="form-control" id="companyname" type="text" name="companyname" value="<?php echo $cname; ?>" required>
          </div>
          <div class="form-group pt-3 mx-auto" style="width: 95%;">
             <label for="ccity">City</label>
-            <input class="form-control" id="ccity" type="text" name="ccity" value="<?php echo $row['ccity']; ?>" required>
+            <input class="form-control" id="ccity" type="text" name="ccity" value="<?php echo $ccity; ?>" required>
          </div>
-      <!-- </div>end ROW -->
-      <!-- <div class="row" style="margin-left: 20px;"> -->
          <div class="form-group pt-3 mx-auto" style="width: 95%;">
             <label for="cstate">State</label>
-            <input class="form-control" id="cstate" type="text" name="cstate" value="<?php echo $row['cstate']; ?>" required>
-         </div>   
-         <div class="form-group pt-3 mx-auto" style="width: 95%;">
-            <label for="fname">czip</label>
-            <input class="form-control" id="czip" type="number" name="czip" value="<?php echo $row['czip']; ?>" required>
-         </div> 
-      <!-- </div> end ROW -->
-      <!-- <div class="row" style="margin-left: 20px;">
-         <div class="form-group pt-3" style="width: 48.6%;">
-            <label for="fname">Password</label>
-            <input class="form-control" id="password" type="password" name="password" value="<?php //echo $row['password']; ?>" required>
-            <i class="bi bi-eye-slash" id="togglePassword" style="cursor: pointer;"></i>  Show/Hide Password
-         </div>   
-         <div class="form-group pt-3" style="width: 48.6%;">
-            <label for="fname">Confirm Password</label>
-            <input class="form-control" id="cpassword" type="password" name="cpassword" value="<?php //echo $row['password']; ?>" required>
+            <input class="form-control" id="cstate" type="text" name="cstate" value="<?php echo $cstate; ?>" required>
          </div>
-      </div> -->
-      <!-- end ROW -->
-
+         <div class="form-group pt-3 mx-auto" style="width: 95%;">
+            <label for="czip">Zip Code</label>
+            <input class="form-control" id="czip" type="number" name="czip" value="<?php echo $czip; ?>" required>
+         </div>
       <div class="form-group pt-3 mx-auto" style="width: 95%; margin-bottom: 10px;">
-      <input type="submit" name="update-company" value="Update User" class="btn btn-secondary">
+      <input type="submit" name="update-company" value="Update Company" class="btn btn-secondary">
       <?php 
-      }
-   } else {
-     echo "0 results";
-   }
+       }
+    } else {
+      echo "0 results";
+    }
       ?>
    </form>
 </div>
@@ -183,6 +170,26 @@ $result = mysqli_query($conn, $update_select);
 
 
 <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+
+<!-- <script>
+   const togglePassword = document.querySelector("#togglePassword");
+        const password = document.querySelector("#password");
+
+        togglePassword.addEventListener("click", function () {
+            // toggle the type attribute
+            const type = password.getAttribute("type") === "password" ? "text" : "password";
+            password.setAttribute("type", type);
+            
+            // toggle the icon
+            this.classList.toggle("bi-eye");
+        });
+
+        // prevent form submit
+        const form = document.querySelector("form");
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+        });
+</script> -->
 
 </body>
 </html>
