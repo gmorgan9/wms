bbacf61228a06823ec2a9dd3de793f58b8f25c4d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-06 09:42:59 -0600

    update

--- admin/actions/add-company.php
+++ admin/actions/add-company.php
@@ -38,6 +38,7 @@ if(isset($_POST['add-company'])){
     }else{
           $insert = "INSERT INTO company (companyname, ccity, cstate, czip) VALUES('$cname', '$ccity', '$cstate', '$czip')";
           mysqli_query($conn, $insert);
+          $_SESSION['companyID'] = $row['companyID'];
           header('location: /admin/actions/add-company.php');
        }
  
