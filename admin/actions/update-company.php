143bcf9c14ea749ef108ac1cd1684a9e31583a98
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-06 09:34:19 -0600

    update

--- admin/actions/update-company.php
+++ admin/actions/update-company.php
@@ -15,34 +15,38 @@ if(!isAdmin()){
 }
 
 
-// $compID = $_SESSION['empID'];
-// $select = " SELECT * FROM employee WHERE employeeID = '$empID' ";
+// $empID = $_SESSION['empID'];
+// $select = " SELECT * FROM c WHERE employeeID = '$empID' ";
 // $result = mysqli_query($conn, $select);
 
 if(isset($_POST['update-company'])){
 
-    $compID   = mysqli_real_escape_string($conn, $_POST['companyID']);
-    $cname = mysqli_real_escape_string($conn, $_POST['companyname']);
-    $ccity = mysqli_real_escape_string($conn, $_POST['ccity']);
-    $cstate = mysqli_real_escape_string($conn, $_POST['cstate']);
-    $czip = mysqli_real_escape_string($conn, $_POST['czip']);
- 
-    $select = " SELECT * FROM company WHERE companyname = '$cname'";
-    $update_result = mysqli_query($conn, $select);
- 
-    if(mysqli_num_rows($update_result) > 0){
- 
-       // $error[] = 'user already exist!';
-       $update = "UPDATE company SET companyname = '$cname', ccity = '$ccity', cstate = '$cstate', czip = '$czip' where companyID = '$compID' ";
-       mysqli_query($conn, $update);
-       $success[] = 'Success';
-       //header('location:' . BASE_URL . '/admin/companies.php');
-    //    header('Location: ' . $_SERVER['HTTP_REFERER']);
-       
-    }else{
-       $error[] = "Error";
-    } 
- };
+   $compID   = mysqli_real_escape_string($conn, $_POST['companyID']);
+   $cname = mysqli_real_escape_string($conn, $_POST['companyname']);
+   $ccity = mysqli_real_escape_string($conn, $_POST['ccity']);
+   $cstate = mysqli_real_escape_string($conn, $_POST['cstate']);
+   $czip = mysqli_real_escape_string($conn, $_POST['czip']);
+   // $pass = md5($_POST['password']);
+   // $cpass = md5($_POST['cpassword']);
+   // $isadmin = $_POST['isadmin'];
+
+   $update_select = " SELECT * FROM company WHERE companyname = '$cname' ";
+
+   $update_result = mysqli_query($conn, $update_select);
+
+   if(mysqli_num_rows($update_result) > 0){
+
+      // $error[] = 'user already exist!';
+      $update = "UPDATE company SET companyname = '$cname', ccity = '$ccity', cstate = '$cstate', czip = '$czip' where companyID = '$compID' ";
+      mysqli_query($conn, $update);
+      $success[] = 'Success';
+      //header('location:' . BASE_URL . '/admin/profile.php');
+      
+   }else{
+      
+   } 
+};
+
 ?>
 
 <!DOCTYPE html>
@@ -51,7 +55,7 @@ if(isset($_POST['update-company'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>WMS | Update Company</title>
+   <title>WMS | Admin Profile</title>
 
    <!-- Custom Styles -->
    <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/other-style.css?v='. time(); ?>">
@@ -85,80 +89,88 @@ if(isset($_POST['update-company'])){
 <div class="main"> 
    
 <?php 
- if (mysqli_num_rows($update_result) > 0) {
+
+if (mysqli_num_rows($update_result) > 0) {
    while($row = mysqli_fetch_assoc($update_result)) {
-//       $acc_type = $row['acc_type'];
+     // $acc_type = $row['acc_type'];
 ?>
 
   <div class="page-header mx-auto">
-    <p class="page_title" style="float: left; padding-top: 2px;">Add Company</p>
+    <p class="page_title" style="float: left; padding-top: 2px;">Profile</p>
     <ul class="breadcrumb">
       <li><a href="<?php echo BASE_URL . '/pages/dashboard.php' ?>">Dashboard</a></li>
-      <li><a href="<?php echo BASE_URL . '/admin/companies.php' ?>">Companies</a></li>
-      <li>Add Company</li>
+      <li>Admin Profile</li>
     </ul>
   </div>
 
-  <!-- <div class="jumbotron jumbotron-fluid bg-white m-2 mx-auto" style="width: 98%;">
+  <div class="jumbotron jumbotron-fluid bg-white m-2 mx-auto" style="width: 98%;">
   <div class="container">
-    <h3 class="display-6 text-center" style="padding-top: 5px !important;padding-bottom: 10px !important;">Welcome, <span style="text-transform: capitalize;"><?php //echo $row['fname'] ?>!</span></h3>
+    <h3 class="display-6 text-center" style="padding-top: 5px !important;padding-bottom: 10px !important;">Welcome, <span style="text-transform: capitalize;"><?php echo $row['fname'] ?>!</span></h3>
   </div>
-</div> -->
+</div>
 
-<div class="page-content mx-auto mt-2">
+<div class="page-content mx-auto">
 <form action="" method="post">
-      <h3 class="mx-auto" style="width: 95%;">Update Company Data</h3>
+      <h3 class="mx-auto" style="width: 95%;">Admin Profile</h3>
       <?php
-      if(isset($error)){
-         foreach($error as $error){
-            echo '<span class="error-msg">'.$error.'</span>';
-         };
-      };
+      // if(isset($error)){
+      //    foreach($error as $error){
+      //       echo '<span class="error-msg">'.$error.'</span>';
+      //    };
+      // };
 
       if(isset($success)){
          foreach($success as $success){
-            echo '<span class="success-msg">'.$success.'</span>';
+            echo '<span class="error-msg">'.$success.'</span>';
          };
       };
       ?> 
-      <?php
-    //   $sql = "SELECT * FROM company";
-    //   $all = mysqli_query($conn, $sql);
-    //   if($all) {
-    //       while ($row = mysqli_fetch_assoc($all)) {
-    //         $compID   = $row['companyID'];
-    //         $cname    = $row['companyname'];
-    //         $ccity    = $row['ccity'];
-    //         $cstate   = $row['cstate'];
-    //         $czip     = $row['czip'];
-  ?>
-      <div class="form-group pt-3 mx-auto" style="width: 95%;">
+      <div class="row" style="margin-left: 20px;">
+      <div class="form-group pt-3" style="width: 20%;">
             <label for="companyID">Company ID</label>
-            <input class="form-control" style="width: 20%" id="companyID" type="text" value="<?php echo $compID; ?>" name="companyID" disabled>
+            <input class="form-control" style="width: 90%" id="companyID" type="text" value="<?php echo $row['companyID']; ?>" name="companyID" disabled>
          </div>
-      <div class="form-group pt-3 mx-auto" style="width: 95%;">
+
+      <!-- <div class="row" style="margin-left: 20px;"> -->
+         <div class="form-group pt-3 mx-auto" style="width: 95%;">
             <label for="companyname">Company Name</label>
-            <input class="form-control" id="companyname" type="text" name="companyname" value="<?php echo $cname; ?>" required>
+            <input class="form-control" id="companyname" type="text" name="companyname" value="<?php echo $row['companyname']; ?>" required>
          </div>
          <div class="form-group pt-3 mx-auto" style="width: 95%;">
             <label for="ccity">City</label>
-            <input class="form-control" id="ccity" type="text" name="ccity" value="<?php echo $ccity; ?>" required>
+            <input class="form-control" id="ccity" type="text" name="ccity" value="<?php echo $row['ccity']; ?>" required>
          </div>
+      <!-- </div>end ROW -->
+      <!-- <div class="row" style="margin-left: 20px;"> -->
          <div class="form-group pt-3 mx-auto" style="width: 95%;">
             <label for="cstate">State</label>
-            <input class="form-control" id="cstate" type="text" name="cstate" value="<?php echo $cstate; ?>" required>
-         </div>
+            <input class="form-control" id="cstate" type="text" name="cstate" value="<?php echo $row['cstate']; ?>" required>
+         </div>   
          <div class="form-group pt-3 mx-auto" style="width: 95%;">
             <label for="czip">Zip Code</label>
-            <input class="form-control" id="czip" type="number" name="czip" value="<?php echo $czip; ?>" required>
+            <input class="form-control" id="czip" type="number" name="czip" value="<?php echo $row['czip']; ?>" required>
+         </div> 
+      <!-- </div> end ROW -->
+      <!-- <div class="row" style="margin-left: 20px;">
+         <div class="form-group pt-3" style="width: 48.6%;">
+            <label for="fname">Password</label>
+            <input class="form-control" id="password" type="password" name="password" value="<?php //echo $row['password']; ?>" required>
+            <i class="bi bi-eye-slash" id="togglePassword" style="cursor: pointer;"></i>  Show/Hide Password
+         </div>   
+         <div class="form-group pt-3" style="width: 48.6%;">
+            <label for="fname">Confirm Password</label>
+            <input class="form-control" id="cpassword" type="password" name="cpassword" value="<?php //echo $row['password']; ?>" required>
          </div>
+      </div> -->
+      <!-- end ROW -->
+
       <div class="form-group pt-3 mx-auto" style="width: 95%; margin-bottom: 10px;">
       <input type="submit" name="update-company" value="Update Company" class="btn btn-secondary">
       <?php 
-       }
-    } else {
-      echo "0 results";
-    }
+      }
+   } else {
+     echo "0 results";
+   }
       ?>
    </form>
 </div>
