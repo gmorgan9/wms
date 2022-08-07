1ab1250d80b22b938177ca44078ab1001806624f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-07 16:27:27 -0600

    udpate

--- admin/jobs.php
+++ admin/jobs.php
@@ -37,7 +37,7 @@ if(isset($_POST['add-job'])){
   }else{
         
         $insert = "INSERT INTO job (idno, jobtitle, dept_code) VALUES('$idno', '$jobtitle', '$dept_code')";
-        $compdata2 = "INSERT INTO employee_company_data (company_code) SELECT company_code FROM department";
+        $compdata2 = "INSERT INTO employee_company_data (company_code) SELECT company_code FROM department WHERE dept_code= '$dept_code'";
         $compdata = "INSERT INTO employee_company_data (dept_code, job_code) SELECT dept_code, jobID FROM job";
         
         mysqli_query($conn, $insert);
