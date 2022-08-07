353411e0fa553f67a7a5500e5a5dffb36bfb13c3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-06 18:47:53 -0600

    update

--- admin/companies.php
+++ admin/companies.php
@@ -84,7 +84,7 @@ if(isset($_GET['companyID'])) {
 </div> -->
 
 <!-- start PAGE-CONTENT -->
-<div class="page-content d-flex justify-content-end mt-2 w-50">
+<div class="page-content mr-2 mt-2 w-50" style="float: right;">
 <div class="d-grid d-md-flex justify-content-md-end">
   <button class="badge text-bg-secondary" style="border-color: rgba(0,0,0,0);" type="button"><a style="color: white; text-decoration:none;" href="<?php echo BASE_URL . '/admin/actions/add-company.php' ?>"><i class="bi bi-plus-lg"></i> Company</a></button>
   <!-- <button class="btn btn-primary" type="button">Button</button> -->
