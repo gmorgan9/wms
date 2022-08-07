a60a86d3945d802d2628556aedee176e3b704e61
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-07 13:20:17 -0600

    update

--- admin/employees.php
+++ admin/employees.php
@@ -219,7 +219,7 @@ if (mysqli_num_rows($result) > 0) {
 
             <?php 
             $id = $_GET['employeeID'];
-            $select = " SELECT * FROM employee WHERE employeeID = '$id' ";
+            $select = " SELECT * FROM employee WHERE employeeID = '$empID' ";
             $result = mysqli_query($conn, $select);          
             if (mysqli_num_rows($result) > 0) {
                while($row = mysqli_fetch_assoc($result)) {
