b503d65d63d6b3b2137c344f8ff193f77ee9eae2
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-07 11:50:33 -0600

    update

--- admin/actions/edit-employee.php
+++ admin/actions/edit-employee.php
@@ -106,9 +106,9 @@ if (mysqli_num_rows($result) > 0) {
     </ul>
   </div>
 
-<div class="page-content mx-auto mt-2">
+<div class="page-content mx-auto mt-2 w-50">
 <form action="" method="post">
-      <span class="mx-auto text-muted" style="padding-top: 10px; width: 95%;">Personal Details</span>
+      <span class="mx-auto text-muted" style="padding-top: 10px; width: 95%;">Personal Information</span>
       <hr>
       <div class="row" style="margin-left: 20px;">
       <div class="form-group pt-3" style="width: 20%;">
