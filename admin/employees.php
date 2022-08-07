c0533cd78af47cf0b6f7cf92d689d50a22582cd0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-07 13:06:53 -0600

    update

--- admin/employees.php
+++ admin/employees.php
@@ -212,6 +212,7 @@ if (mysqli_num_rows($result) > 0) {
        
         <span class="badge text-bg-danger" style="font-size: 10px;">Be Careful! This will delete all data corresponding with this employee!</span>
         
+        <div class="col">
           <div class="row">
         <?php 
         $id = $_GET['employeeID'];
@@ -330,6 +331,8 @@ if (mysqli_num_rows($result) > 0) {
    }
       ?>
    </form>
+  </div>
+   </div>
   </div>
 
 
