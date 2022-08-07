9f88cf60aafb12b3808fde796b5b7031ee2ba8dc
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-06 19:12:01 -0600

    update

--- admin/companies.php
+++ admin/companies.php
@@ -111,7 +111,7 @@ if(isset($_GET['companyID'])) {
 
 
 <!-- start PAGE-CONTENT -->
-<div class="page-content mt-2 float-start" style="width: 35%; margin-left: -60px;">
+<div class="page-content mt-2 mx-auto float-start" style="width: 35%; margin-left: -60px;">
   <form action="" method="post">
     <!-- <h6 class="mx-auto" style="width: 95%;">Add Company</h6> -->
     <div class="form-group pt-3 mx-auto" style="width: 95%;">
