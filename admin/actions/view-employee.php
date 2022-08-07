ce86aad1ffcd60893227c29005fa926f9d291a9e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-06 18:39:25 -0600

    update

--- admin/actions/view-employee.php
+++ admin/actions/view-employee.php
@@ -101,7 +101,7 @@ if (mysqli_num_rows($result) > 0) {
     <p class="page_title" style="float: left; padding-top: 2px;">Profile</p>
     <ul class="breadcrumb">
       <li><a href="<?php echo BASE_URL . '/pages/dashboard.php' ?>">Dashboard</a></li>
-      <li><a href="<?php echo BASE_URL . '/pages/employees.php' ?>">Employees</a></li>
+      <li><a href="<?php echo BASE_URL . '/admin/employees.php' ?>">Employees</a></li>
       <li>Viewing Employee: <span class="text-muted" style="text-transform: capitalize"><?php echo $row['fname']; ?>  </span></li>
     </ul>
   </div>
