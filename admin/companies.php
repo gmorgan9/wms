3abdb33ab28fd12a8e37f23e5db3e1f4716842a8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-06 19:14:21 -0600

    update

--- admin/companies.php
+++ admin/companies.php
@@ -132,7 +132,7 @@ if(isset($_GET['companyID'])) {
 
 
 <!-- start PAGE-CONTENT -->
-<div class="page-content mt-2 float-end" style="width: 65%; margin-right: 2%;">
+<div class="page-content mt-2 float-end" style="width: 65%; margin-right: 2px;">
     <table class="table">
   <thead>
     <tr>
