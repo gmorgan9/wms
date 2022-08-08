38af098fd1a9de5f4f912bd49b47f2e738eb7586
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-07 18:07:00 -0600

    update

--- admin/jobs.php
+++ admin/jobs.php
@@ -35,9 +35,9 @@ if(isset($_POST['add-job'])){
 
   }else{
         $insert = "INSERT INTO job (idno, jobtitle, dept_code) VALUES('$idno', '$jobtitle', '$dept_code')";
-        $compdata = "INSERT INTO employee_company_data (employee_code, dept_code, job_code) SELECT employee_code, dept_code, jobID FROM job";
+        // $compdata = "INSERT INTO employee_company_data (employee_code, dept_code, job_code) SELECT employee_code, dept_code, jobID FROM job";
         mysqli_query($conn, $insert);
-        mysqli_query($conn, $compdata);
+        // mysqli_query($conn, $compdata);
         header('location: /admin/jobs.php');
      }
 
