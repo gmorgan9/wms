c9b63b6048baa6a51309c7f5a9554aaa2e06e1af
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-08 14:27:24 -0600

    udpate

--- admin/actions/view-employee.php
+++ admin/actions/view-employee.php
@@ -71,7 +71,7 @@ if (mysqli_num_rows($result) > 0) {
       <div class="col-md-8 float-start w-25 ms-4">
               <div class="card mb-3">
                 <div class="card-body">
-                  <img src="../assets/img/pic_holder.jpg" alt="">
+                  <img src="../../assets/img/pic_holder.jpg" alt="">
                   </div>
                 </div>
               </div>
