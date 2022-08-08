c0fc2855d2e145e5d336bab90dd8d78c04cc25f4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-07 19:33:28 -0600

    update

--- admin/jobs.php
+++ admin/jobs.php
@@ -149,7 +149,7 @@ if(isset($_GET['jobID'])) {
       $all = mysqli_query($conn, $sql);
       if($all) {
           while ($row = mysqli_fetch_assoc($all)) {
-            $jobID   = $row['companyID'];
+            $jobID   = $row['jobID'];
             $idno     = $row['idno'];
             $jobtitle    = $row['jobtitle'];
   ?>
