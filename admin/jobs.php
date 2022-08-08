35ed0f7da3c20b8cb59ed67edd5f93c364fad0b1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-08 11:56:26 -0600

    update

--- admin/jobs.php
+++ admin/jobs.php
@@ -254,10 +254,10 @@ if(isset($_GET['jobID'])) {
       <?php 
       // if(isset($_GET['viewID'])) {
         //$id = $_GET['jobID'];
-          $sql = "SELECT * FROM job where idno = '$idno'";
-          $all = mysqli_query($conn, $sql);
-          if($all) {
-              while ($row = mysqli_fetch_assoc($all)) {
+          // $sql = "SELECT * FROM job where idno = '$idno'";
+          $result = mysqli_query($conn, $sql);
+          if($result) {
+              while ($row = mysqli_fetch_assoc($result)) {
                 //$jobID       = $row['jobID'];
                 //$idno        = $row['idno'];
                 $jobtitle    = $row['jobtitle'];
