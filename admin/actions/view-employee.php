470a7ca1c5e9771908d8f08c27256f2ac18c9471
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-08 14:20:35 -0600

    udpate

--- admin/actions/view-employee.php
+++ admin/actions/view-employee.php
@@ -68,7 +68,7 @@ if (mysqli_num_rows($result) > 0) {
       <h3 class="mx-auto" style="width: 95%;">Employee View</h3>
 
 
-      <div class="col-md-8 float-start w-25 ms-5">
+      <div class="col-md-8 float-start w-25 ms-4">
               <div class="card mb-3">
                 <div class="card-body">
                   image place holder
