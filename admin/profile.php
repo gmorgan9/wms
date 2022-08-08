90ec4088c3d4bd808829286ff4aac146ad6535cd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-08 15:18:20 -0600

    update

--- admin/profile.php
+++ admin/profile.php
@@ -178,14 +178,17 @@ if (mysqli_num_rows($result) > 0) {
                         <span class="text-danger">Inactive</span>
                      <?php } ?>
                     </div>
-                    <div class="col-sm-5">
-                      <h6 class="mb-0">Employment Status</h6>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Account Type</h6>
                     </div>
                     <div class="col-sm-9 text-secondary">
-                    <?php if($row['status'] == 1) { ?>
-                        <span class="text-success">Active</span>
-                     <?php } if($row['status'] == 0) { ?>
-                        <span class="text-danger">Inactive</span>
+                    <?php if($row['acc_type'] == 1) { ?>
+                        <span class="text-success">Admin</span>
+                     <?php } if($row['acc_type'] == 0) { ?>
+                        <span class="text-danger">Employee</span>
                      <?php } ?>
                     </div>
                   </div>
