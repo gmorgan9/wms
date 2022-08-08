651801b8dd9e6f7bc76a31f741fb6a6522dfe203
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-07 19:52:17 -0600

    update

--- admin/companies.php
+++ admin/companies.php
@@ -205,7 +205,7 @@ if(isset($_GET['companyID'])) {
       </div>
       <div class="modal-footer">
         <a class="badge text-bg-primary" style="text-decoration: none; cursor: pointer;" data-bs-dismiss="modal">Cancel</a>
-        <a class="badge text-bg-danger" style="text-decoration: none; cursor: pointer;" href="companies.php?companyID=<?php echo $compID; ?>">Delete</a>
+        <a class="badge text-bg-danger" style="text-decoration: none; cursor: pointer;" href="companies.php?companyID=<?php echo $id; ?>">Delete</a>
         <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
         <a href=""></a>
       </div>
