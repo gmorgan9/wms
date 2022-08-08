0ffff4892c15d7b3a808a15b47ffd7b47f471cf9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-07 20:07:50 -0600

    update

--- admin/companies.php
+++ admin/companies.php
@@ -162,7 +162,7 @@ if(isset($_GET['companyID'])) {
         <!-- <td><?php //echo $ccity; ?></td>
         <td><?php //echo $cstate; ?></td>
         <td><?php //echo $czip; ?></td> -->
-        <td><a onclick="deleteconfig()" style="text-decoration: none;"  class="badge text-bg-danger" href="companies.php?companyID=<?php echo $compID; ?>">Delete</a></td>
+        <td><a onclick="deleletconfig()" style="text-decoration: none;"  class="badge text-bg-danger" href="companies.php?companyID=<?php echo $compID; ?>">Delete</a></td>
         <?php } ?>
 
         <script>
