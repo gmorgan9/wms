380d1c72144ca2f7f583ffdb9751fc9f2591e5ae
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-09 10:04:19 -0600

    update

--- admin/employees.php
+++ admin/employees.php
@@ -178,7 +178,7 @@ if (mysqli_num_rows($result) > 0) {
           <a style="text-decoration: none;" class="badge text-bg-success" href="actions/view-employee.php?employeeID=<?php echo $empID; ?>">View</a>
           <a style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#confirmDelete" class="badge text-bg-danger" href="employees.php?employeeID=<?php echo $empID; ?>">Delete</a>
         </td>
-        <?php } else { }}}?>
+        <?php } }}?>
   </tbody>
 </table>
       <?php 
