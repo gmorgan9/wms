75c0d8384400d09106f6baef717d616e23f1607d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-09 14:07:30 -0600

    update

--- pages/actions/view-timesheet.php
+++ pages/actions/view-timesheet.php
@@ -258,7 +258,7 @@ if (mysqli_num_rows($result) > 0) {
                   <hr>
                   <div class="row">
                     <div class="col-sm-3">
-                      <h6 class="mb-0">Actions</h6>
+                      <h6 class="mb-0">Reason</h6>
                     </div>
                     <div class="col-sm-9 text-secondary">
                     <?php echo $row['reason']; ?>
