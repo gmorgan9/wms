8327c004438605ef6976263345e4254d5a96c1ba
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-09 17:42:30 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -95,7 +95,14 @@ if(!isLoggedIn()){
 </div>
 
 
+
 </div>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <label>Select Image File:</label>
+    <input type="file" name="image">
+    <input type="submit" name="submit" value="Upload">
+</form>
 
 
 
