714f54babd92f16e852cdd9682c8a8fbe3fbe4d0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-09 11:36:55 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -51,9 +51,9 @@
                 <a href="<?php echo BASE_URL . '/admin/jobs.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
                     <i class="bi bi-clock-history"></i>
                     <?php if($rowcount == 0){ ?>
-                        <span>  Timesheet Changes</span>
+                        <span>  Time Changes</span>
                     <?php } else { ?>
-                        <span>  Timesheet Changes</span> &nbsp;  <span class="badge rounded-pill text-bg-danger" style="margin-top: -10px !important;"><?php echo $rowcount; ?></span>
+                        <span>  Time Changes</span> &nbsp;  <span class="badge rounded-pill text-bg-danger" style="margin-top: -10px !important;"><?php echo $rowcount; ?></span>
                     <?php }} ?>
                 </a>
                 <?php } ?>
