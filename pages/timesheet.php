299630834bfddf861f65b8a48ac30d733d982cd6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-09 10:05:57 -0600

    update

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -118,7 +118,7 @@ if(isset($_GET['jobID'])) {
 <div class="page-content float-start" style="margin-top: 12px; width: 32%;margin-left: -45px; height: unset !important;">
   <form action="" method="post">
     <div class="section-header pt-2">
-      <span class="text-muted pt-4" style="width: 95%;">Job Requests</span>
+      <span class="text-muted pt-4" style="width: 95%;">Time Entry</span>
     </div>
     <hr style="margin-bottom: -5px; margin-top: 5px;">
     <?php 
