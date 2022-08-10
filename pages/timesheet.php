bf88afdc92ca0dbbcca47a4e49e5bb272c485c0b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-10 14:13:35 -0600

    update

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -304,7 +304,7 @@ if(isset($_GET['timeID'])) {
 
           ?>
 
-              <?php if($totalmin > 59) { ?>
+              <?php if($totalmin >= 59) { ?>
                 <?php echo $totalmin; ?> Min
               <?php } else { ?>
                 <?php echo $totalhrs; ?> Hrs
