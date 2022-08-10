81a2be37186170a929c5da4d7ee74e393917c5e6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-10 14:49:10 -0600

    update

--- pages/info.php
+++ pages/info.php
@@ -172,7 +172,7 @@ if (mysqli_num_rows($result) > 0) {
         <!-- <input class="form-control" id="employee_idno" type="hidden" name="employee_idno" value="<?php //echo $employee_idno; ?>"> -->
         <div class="form-group pt-3 mx-auto" style="width: 95%;">
       <label for="companyname" style="font-size: 14px;">Company <span class="text-muted" style="font-size: 10px;">e.g. "Apple Corporation"</span></label>
-      <input class="form-control" id="companyname" type="text" name="companyname" value="" required>
+      <input class="form-control" id="companyname" type="text" name="companyname" value="<?php echo $row['comapnyname']; ?>" required>
     </div>
     <div class="form-group pt-3 mx-auto" style="width: 95%;">
       <label for="deptname" style="font-size: 14px;">Department <span class="text-muted" style="font-size: 10px;">e.g. "Accounting"</span></label>
