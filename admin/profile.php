aea1e5540e0b3d369a9105a31b10c6460419c3fa
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-09 21:55:20 -0600

    update

--- admin/profile.php
+++ admin/profile.php
@@ -74,7 +74,6 @@ $result = mysqli_query($conn, $select);
 if (mysqli_num_rows($result) > 0) {
    while($row = mysqli_fetch_assoc($result)) {
       $acc_type = $row['acc_type'];
-      $employee_idno = $row['idno'];
 ?>
 
   <div class="page-header mx-auto">
@@ -85,38 +84,28 @@ if (mysqli_num_rows($result) > 0) {
     </ul>
   </div>
 
-  <?php
-                  $empidno = $_SESSION['employee_idno'];
-                  $select = "SELECT * FROM job WHERE employee_idno = '$empidno' ";
-                  $result = mysqli_query($conn, $select);
-                  if (mysqli_num_rows($result) > 0) {
-                     while($row = mysqli_fetch_assoc($result)) {
 
-                  ?>
-                      <?php }} ?>   
 
 
 <div class="page-content mx-auto mt-2">
-  <form action="" method="post">
-    <h3 class="mx-auto" style="width: 95%;">My Profile</h3>
+<form action="" method="post">
+      <h3 class="mx-auto" style="width: 95%;">My Profile</h3>
 
     <div class="row">
-      
-    
       <div class="col-md-8 float-start w-25 ms-4">
-        <div class="card mb-3">
-          <div class="card-body">
-            <img class="ms-1" src="../../assets/img/pic_holder.jpg" style="height: 250px; width: 250px; border-radius: 150px;" alt="">
-          </div>
-        </div>
-      </div>
-
-              
-
-            <div class="col-md-8 float-end me-4">
               <div class="card mb-3">
                 <div class="card-body">
-                  <div class="row">
+                  <img class="ms-1" src="../../assets/img/pic_holder.jpg" style="height: 250px; width: 250px; border-radius: 150px;" alt="">
+                  </div>
+                </div>
+              </div>
+
+
+
+      <div class="col-md-8 float-end me-4">
+              <div class="card mb-3">
+                <div class="card-body">
+                <div class="row">
                     <div class="col-sm-3">
                       <h6 class="mb-0">Account Type</h6>
                     </div>
@@ -188,8 +177,7 @@ if (mysqli_num_rows($result) > 0) {
                   </div>
                 </div>
               </div>
-            </div>
-                     </div>
+              </div>
               <?php 
       }
    } else {
