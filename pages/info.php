f24f7765807611f3eccdc387354964c32ac0626e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-10 14:33:08 -0600

    update

--- pages/info.php
+++ pages/info.php
@@ -48,7 +48,127 @@ if(!isLoggedIn()){
       <li>Information</li>
     </ul>
   </div>
+
+  <?php 
+
+$empID = $_SESSION['empID'];
+$select = " SELECT * FROM employee WHERE employeeID = '$empID' ";
+$result = mysqli_query($conn, $select);
+if (mysqli_num_rows($result) > 0) {
+   while($row = mysqli_fetch_assoc($result)) {
+      $acc_type = $row['acc_type'];
+?>
+
+
+  <div class="page-content mx-auto mt-2">
+<form action="" method="post">
+      <h3 class="mx-auto" style="width: 95%;">My Profile</h3>
+
+
+      <div class="col-md-8 float-start w-25 ms-4">
+              <div class="card mb-3">
+                <div class="card-body">
+                  <img class="ms-1" src="../../assets/img/pic_holder.jpg" style="height: 250px; width: 250px; border-radius: 150px;" alt="">
+                  </div>
+                </div>
+              </div>
+
+
+
+      <div class="col-md-8 float-end me-4">
+              <div class="card mb-3">
+                <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Account Type</h6>
+                    </div>
+                    <div class="col-sm-9 text-info">
+                    <?php if($row['acc_type'] == 1) { ?>
+                        Admin
+                     <?php } if($row['acc_type'] == 0) { ?>
+                        Employee
+                     <?php } ?>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Employee</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <span class="text-capitalize"><?php echo $row['lname']; ?>, <?php echo $row['fname']; ?> &nbsp; (<?php echo $row['idno']; ?>)</span>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Username</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                      <?php echo $row['uname']; ?>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Email <span class="text-muted" style="font-size: 10px;">Personal</span></h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                     <?php echo $row['email']; ?>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Gender</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <span class="text-capitalize"><?php echo $row['gender']; ?></span>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Employment Status</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <?php if($row['status'] == 1) { ?>
+                        <span class="text-success">Active</span>
+                     <?php } if($row['status'] == 0) { ?>
+                        <span class="text-danger">Inactive</span>
+                     <?php } ?>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Actions</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <a style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#editEmployee" class="badge text-bg-primary" href="#">Edit</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <?php 
+      }
+   } else {
+     echo "0 results" . $_SESSION['empID'];
+   }
+      ?>
+   </form>
 </div>
+
+
+
+
+
+
+
+
+</div>
+<!-- END MAIN -->
+
 
 
 <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
