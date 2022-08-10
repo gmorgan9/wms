649907ffe62f1109c468be20abc0dab301b124a6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-10 12:03:31 -0600

    update

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -279,7 +279,7 @@ if(isset($_GET['timeID'])) {
       $sql = "SELECT * FROM timesheet where employee_idno = '{$_SESSION['employee_idno']}'";
       $all = mysqli_query($conn, $sql);
       if($all) {
-          while ($row = mysqli_fetch_assoc($all)) {
+          while ($row = mysqli_fetch_field($all)) {
             $timeID           = $row['timeID'];
             $idno             = $row['idno'];
             $orgDate          = $row['date'];
