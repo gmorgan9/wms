bdf1f29771e118936d1b18f7ac12d4344535534a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-10 08:10:46 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -6,6 +6,7 @@ $select = " SELECT * FROM employee WHERE employeeID = '$empID' ";
 $result = mysqli_query($conn, $select);
 if (mysqli_num_rows($result) > 0) {
    while($row = mysqli_fetch_assoc($result)) {
+    $fname = $row['fname'];
 }}
 ?>
 
@@ -30,10 +31,10 @@ if (mysqli_num_rows($result) > 0) {
         <ul class="navbar-nav ms-auto d-flex flex-row">
             <?php if(isset($_SESSION['fname'])){ ?>
             <?php if($row['acc_type'] == 1){ ?>
-                <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $row['fname']; ?></span>!</a></li>
+                <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $fname; ?></span>!</a></li>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/admin/profile.php' ?>"><i class="bi bi-person"></i>  Profile</a></li>
             <?php } else { ?>
-                <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $row['fname']; ?></span>!</a></li>
+                <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $fname; ?></span>!</a></li>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/pages/profile.php' ?>"><i class="bi bi-person"></i>  Profile</a></li>
             <?php }?>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/logout.php' ?>">Logout</a></li>
