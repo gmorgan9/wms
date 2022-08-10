9bc74fd945bb7a0220a9530c53a32cced4477ccc
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-09 18:35:13 -0600

    update

--- pages/profile.php
+++ pages/profile.php
@@ -288,7 +288,7 @@ if (mysqli_num_rows($result) > 0) {
       </div>
       <div class="modal-footer">
         <div class="form-group pt-3" style="margin-bottom: 10px;">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" style="border-color: rgba(0,0,0,0);" class="badge text-bg-secondary" data-bs-dismiss="modal">Close</button>
           <button type="submit" style="border-color: rgba(0,0,0,0);" name="update-time" class="badge text-bg-secondary">Update Profile</button>
         </div>
       </div>
