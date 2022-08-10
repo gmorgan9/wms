6c6083bab8efee0cc9b222526216a196105b88da
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-10 08:08:32 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -30,15 +30,15 @@ if (mysqli_num_rows($result) > 0) {
 
         <ul class="navbar-nav ms-auto d-flex flex-row">
             <?php if(isset($_SESSION['fname'])){ ?>
-            <?php if($row['acc_type'] == 1){ ?>
-                <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $row['fname']; ?></span>!</a></li>
-                <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/admin/profile.php' ?>"><i class="bi bi-person"></i>  Profile</a></li>
-            <?php } else { ?>
-                <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $row['fname']; ?></span>!</a></li>
-                <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/pages/profile.php' ?>"><i class="bi bi-person"></i>  Profile</a></li>
-            <?php }}}?>
+                <?php if($row['acc_type'] == 1){ ?>
+                    <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $row['fname']; ?></span>!</a></li>
+                    <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/admin/profile.php' ?>"><i class="bi bi-person"></i>  Profile</a></li>
+                <?php } else { ?>
+                    <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $row['fname']; ?></span>!</a></li>
+                    <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/pages/profile.php' ?>"><i class="bi bi-person"></i>  Profile</a></li>
+                <?php }?>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/logout.php' ?>">Logout</a></li>
-            <?php }
+            <?php }}}
             if(!isset($_SESSION['fname'])) { ?>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="/">Home</a></li>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="pages/entry/login.php">Login/Signup</a></li>
