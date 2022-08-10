d9518e31002050316e44eeb57904c94c67282b47
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-10 08:07:40 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -15,7 +15,9 @@ if (mysqli_num_rows($result) > 0) {
 <!-- Navbar -->
     <?php if(isset($_SESSION['fname'])){ ?>
         <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-    <?php } else { ?>
+    <?php } 
+    
+    if(!isset($_SESSION['fname'])){ ?>
         <nav id="main-navbar" class="navbar navbar-expand-lg navbar-dark fixed-top">
     <?php }?>
       <!-- Container wrapper -->
