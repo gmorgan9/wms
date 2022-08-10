8a6418bc44ffdd3fd6c6c47795d96c0804b75203
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-10 14:10:39 -0600

    upda

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -300,7 +300,7 @@ if(isset($_GET['timeID'])) {
           <?php
           //$totalhrs = $timeout - $timein;
           $totalhrs = round((strtotime($timeout) - strtotime($timein)) / 3600, 1);
-          $totalmin = round((strtotime($timeout) - strtotime($timein)) / 60, 2);
+          $totalmin = round((strtotime($timeout) - strtotime($timein)) / 60);
 
           ?>
 
