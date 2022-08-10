83962a1d174dd3246a68744a618421d215d9fe60
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-09 18:08:45 -0600

    update

--- pages/profile.php
+++ pages/profile.php
@@ -61,6 +61,9 @@ if(isset($_POST['update-profile'])){
 <!-- Bootstrap Icons -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
+<!-- scripts -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
 <body>
 
