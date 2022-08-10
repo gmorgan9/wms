7ba53a1363e31dc70b826b05259990fa272f6261
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-09 19:15:07 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,8 +1,19 @@
 <!-- WORKING -->
+
+<?php
+$empID = $_SESSION['empID'];
+$select = " SELECT * FROM employee WHERE employeeID = '$empID' ";
+$result = mysqli_query($conn, $select);
+if (mysqli_num_rows($result) > 0) {
+   while($row = mysqli_fetch_assoc($result)) {
+?>
+
+
+
 <!--Main Navigation-->
 <header>
 <!-- Navbar -->
-    <?php if(isset($_SESSION['fname'])){ ?>
+    <?php if(isset($row['fname'])){ ?>
         <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
     <?php } else { ?>
         <nav id="main-navbar" class="navbar navbar-expand-lg navbar-dark fixed-top">
@@ -16,12 +27,12 @@
         </a>
 
         <ul class="navbar-nav ms-auto d-flex flex-row">
-            <?php if(isset($_SESSION['fname'])){ ?>
-            <?php if($_SESSION['acc_type'] == 1){ ?>
-                <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $_SESSION['fname']; ?></span>!</a></li>
+            <?php if(isset($row['fname'])){ ?>
+            <?php if($row['acc_type'] == 1){ ?>
+                <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $row['fname']; ?></span>!</a></li>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/admin/profile.php' ?>"><i class="bi bi-person"></i>  Profile</a></li>
             <?php } else { ?>
-                <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $_SESSION['fname']; ?></span>!</a></li>
+                <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $row['fname']; ?></span>!</a></li>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/pages/profile.php' ?>"><i class="bi bi-person"></i>  Profile</a></li>
             <?php }?>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/logout.php' ?>">Logout</a></li>
@@ -41,3 +52,4 @@
   <div class="container pt-4"></div>
 </main> -->
 <!--Main layout-->
+<?php }} ?>
