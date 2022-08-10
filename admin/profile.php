4d8b460b4deeeb175c705e3453a0be903486aeb5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-09 19:11:06 -0600

    update

--- admin/profile.php
+++ admin/profile.php
@@ -12,9 +12,7 @@ if(!isLoggedIn()){
 }
 
 
-$empID = $_SESSION['empID'];
-$select = " SELECT * FROM employee WHERE employeeID = '$empID' ";
-$result = mysqli_query($conn, $select);
+
 
 if(isset($_POST['update-profile'])){
 
@@ -68,6 +66,9 @@ if(isset($_POST['update-profile'])){
 <div class="main"> 
 <?php 
 
+$employID = $_SESSION['empID'];
+$select = " SELECT * FROM employee WHERE employeeID = '$employID' ";
+$result = mysqli_query($conn, $select);
 if (mysqli_num_rows($result) > 0) {
    while($row = mysqli_fetch_assoc($result)) {
       $acc_type = $row['acc_type'];
