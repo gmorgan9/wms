a985f46a2c07459847122efa5f6c17a9c692f4ba
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-11 09:20:04 -0600

    update

--- admin/jobs.php
+++ admin/jobs.php
@@ -56,14 +56,6 @@ if(isset($_GET['jobID'])) {
     }
   }
 
-  if (isset($_POST['terminated']))
-    {
-        $terUpdateQuery = "UPDATE job SET approval_status = 'terminated' WHERE jobID = '".$_POST['jobID']."'";
-        $terUpdateResult = mysqli_query($conn, $terUpdateQuery);
-        header('location: jobs.php');
-        // $appInsertQuery = "INSERT INTO approved(id,status) VALUES ('".$_POST['row_id']."','Approved')";
-        // $appInsertResult = mysqli_query($conn, $appInsertQuery);
-    }
   
   
   if (isset($_POST['approved']))
@@ -234,10 +226,7 @@ if(isset($_GET['jobID'])) {
         <td><?php echo $jobtitle; ?></td>
         <td>
           <a style="text-decoration: none;" class="badge text-bg-success" href="actions/view-job.php?jobID=<?php echo $jobid; ?>">View</a>
-          <form method="post" action="">
-          <input type="hidden" name="jobID" value="<?php echo $jobid; ?>" />
-          <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="terminated"><span class="badge text-bg-danger">Delete</span></button>
-        </form>
+          <a onclick="return confirm('Be Careful! \r\nOK to delete?')" style="text-decoration: none;" class="badge text-bg-danger" href="jobs.php?jobID=<?php echo $jobid; ?>">Delete</a>
         </td>
         <?php } ?>
         
--- pages/job_request.php
+++ pages/job_request.php
@@ -206,7 +206,7 @@ if(isset($_GET['jobID'])) {
         <td>
           <form method="post" action="">
             <input type="hidden" name="jobID" value="<?php echo $jobID; ?>" />
-            <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="terminated"><span class="badge text-bg-danger">Delete</span></button>
+            <button onclick="return confirm('Be Careful, Can\'t be undone! \r\nOK to delete?')" style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="terminated"><span class="badge text-bg-danger">Delete</span></button>
           </form>
         </td>
         <?php } ?>
