184b95d6e8d49ed5c2436a0660c5eea960d67154
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-11 07:39:11 -0600

    update

--- pages/info.php
+++ pages/info.php
@@ -221,6 +221,15 @@ if(!isLoggedIn()){
                       <!-- <a class="text-decoration-none badge text-bg-success" data-bs-toggle="modal" data-bs-target="#exampleModal" href="#">Edit</a> -->
                       </div>
                     </div>
+                    <hr>
+                    <div class="row">
+                      <div class="col-sm-3">
+                        <h6 class="mb-0">View All Jobs</h6>
+                      </div>
+                      <div class="col-sm-9 text-warning">
+                      <!-- <a class="text-decoration-none badge text-bg-success" data-bs-toggle="modal" data-bs-target="#exampleModal" href="#">Edit</a> -->
+                      </div>
+                    </div>
                   </div>
                 </div>
                 
