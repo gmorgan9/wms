54765c200e2ce89045a738d7cf8f0106b5b9441d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-11 09:21:49 -0600

    update

--- admin/jobs.php
+++ admin/jobs.php
@@ -209,7 +209,7 @@ if(isset($_GET['jobID'])) {
   <tbody class="table-group-divider">
 
   <?php
-      $sql = "SELECT * FROM job";
+      $sql = "SELECT * FROM job WHERE approval_status != 'pending'";
       $all = mysqli_query($conn, $sql);
       if($all) {
           while ($row = mysqli_fetch_assoc($all)) {
