eabe7523ebf20d6a73f643fb60adab21184ca89b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-11 11:35:45 -0600

    update

--- pages/actions/view-timesheet.php
+++ pages/actions/view-timesheet.php
@@ -1,9 +1,11 @@
 <!-- WORKING -->
 <?php
 
-require_once "../../app/database/connection.php";
-require_once "../../app/database/functions.php";
-require_once "../../path.php";
+// REQUIRE
+  require_once "../../app/database/connection.php";
+  require_once "../../app/database/functions.php";
+  require_once "../../path.php";
+// END REQUIRE
 
 session_start();
 
@@ -44,16 +46,19 @@ if(!isLoggedIn()){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>WMS | View Job</title>
 
-   <!-- Custom Styles -->
-   <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/other-style.css?v='. time(); ?>">
+  <!-- START SCRIPTS -->
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/other-style.css?v='. time(); ?>">
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
-<!-- Bootstrap Icons -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
-<!-- scripts -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  <!-- END SCRIPTS -->
 
 </head>
 <body>
