bcfa501f2fa6587dc74c5f0ac980819fd9f0bfbd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-11 08:03:15 -0600

    udpate

--- admin/actions/view-job.php
+++ admin/actions/view-job.php
@@ -214,16 +214,24 @@ if (mysqli_num_rows($result) > 0) {
         <input class="form-control" id="employee_lname" type="hidden" name="employee_lname" value="<?php echo $lname; ?>">
         <input class="form-control" id="employee_idno" type="hidden" name="employee_idno" value="<?php echo $employee_idno; ?>">
       <div class="form-group pt-3 mx-auto" style="width: 95%;">
-        <label for="companyname" style="font-size: 14px;">Company <span class="text-muted" style="font-size: 10px;"></span></label>
+        <label for="companyname" style="font-size: 14px;">Company <span class="text-muted" style="font-size: 10px;">e.g. "Apple Corporation"</span></label>
         <input class="form-control" id="companyname" type="text" name="companyname" value="<?php echo $row['companyname']; ?>" required>
       </div>
       <div class="form-group pt-3 mx-auto" style="width: 95%;">
-        <label for="new_timein" style="font-size: 14px;">Time In <span class="text-muted" style="font-size: 10px;">e.g. "hh:mm"</span></label>
-        <input class="form-control" id="new_timein" type="time" name="new_timein" value="<?php echo $row['timein']; ?>" required>
+        <label for="deptname" style="font-size: 14px;">Department <span class="text-muted" style="font-size: 10px;">e.g. "Accounting"</span></label>
+        <input class="form-control" id="deptname" type="text" name="deptname" value="<?php echo $row['deptname']; ?>" required>
       </div>
       <div class="form-group pt-3 mx-auto" style="width: 95%;">
-        <label for="new_timeout" style="font-size: 14px;">Time Out <span class="text-muted" style="font-size: 10px;">e.g. "hh:mm"</span></label>
-        <input class="form-control" id="new_timeout" type="time" name="new_timeout" value="<?php echo $row['timeout']; ?>" required>
+        <label for="jobtitle" style="font-size: 14px;">Job Title / Position <span class="text-muted" style="font-size: 10px;">e.g. "Cheif Executive Officer"</span></label>
+        <input class="form-control" id="jobtitle" type="text" name="jobtitle" value="<?php echo $row['jobtitle']; ?>" required>
+      </div>
+      <div class="form-group pt-3 mx-auto" style="width: 95%;">
+        <label for="start_date" style="font-size: 14px;">Start Date</label>
+        <input class="form-control" id="start_date" type="date" name="start_date" value="<?php echo $row['start_date']; ?>">
+      </div>
+      <div class="form-group pt-3 mx-auto" style="width: 95%;">
+        <label for="end_date" style="font-size: 14px;">End Date</label>
+        <input class="form-control" id="end_date" type="date" name="end_date" value="<?php echo $row['end_date']; ?>">
       </div>
       <div class="form-group pt-3 mx-auto" style="width: 95%;">
         <label for="reason" style="font-size: 14px;">Reason <span class="text-muted" style="font-size: 10px;">Give reason for changing time</span></label>
