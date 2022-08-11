3d25d340f840d62d0ef5862345822b0dee1bfd5c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-11 11:29:15 -0600

    update

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -171,7 +171,7 @@ if(isset($_GET['timeID'])) {
     <thead>
       <tr>
         <th scope="col" style="font-size: 14px;">Timesheet ID</th>
-        <!-- <th scope="col" style="font-size: 14px;">Type</th> -->
+        <th scope="col" style="font-size: 14px;">Type</th>
         <th scope="col" style="font-size: 14px;">Actions</th>
       </tr>
     </thead>
