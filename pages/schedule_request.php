77b2c2b0bb0f699172ff21627d7df86fe59ab882
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-12 14:05:53 -0600

    update

--- pages/schedule_request.php
+++ pages/schedule_request.php
@@ -157,11 +157,13 @@ if(!isLoggedIn()){
           <?php }?>
           <!-- <td><?php //echo $companyname; ?></td> -->
           <td>
-          <a style="text-decoration: none;" class="badge text-bg-success" href="actions/view-schedule.php?scheduleID=<?php echo $scheduleID; ?>">View</a>
-            <form method="post" action="">
-              <input type="hidden" name="jobID" value="<?php echo $jobID; ?>" />
-              <button onclick="return confirm('Be Careful, Can\'t be undone! \r\nOK to delete?')" style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="terminated"><span class="badge text-bg-danger">Delete</span></button>
-            </form>
+            <div class="d-inline">
+              <a style="text-decoration: none;" class="badge text-bg-success" href="actions/view-schedule.php?scheduleID=<?php echo $scheduleID; ?>">View</a>
+              <form method="post" action="">
+                <input type="hidden" name="jobID" value="<?php echo $jobID; ?>" />
+                <button onclick="return confirm('Be Careful, Can\'t be undone! \r\nOK to delete?')" style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="terminated"><span class="badge text-bg-danger">Delete</span></button>
+              </form>
+            </div>
           </td>
           <?php } ?>
           
