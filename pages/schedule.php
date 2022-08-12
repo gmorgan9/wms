83612eef8ed61ec8d7a8c1384a951e996f2d0aca
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-12 15:26:13 -0600

    update

--- pages/schedule.php
+++ pages/schedule.php
@@ -183,7 +183,7 @@ if(!isLoggedIn()){
   <!-- END SCHEDULE-REQUEST (RIGHT SIDE) -->
   <?php } else { ?>
   <!-- START ADMIN SCHEDULE-REQUEST (RIGHT SIDE) -->
-  <div class="page-content float-start mx-auto" style="margin-top: 12px; width: 32%;margin-left: -100px; height: unset !important;">
+  <div class="page-content float-start" style="margin-top: 12px; width: 32%;margin-left: -90px; height: unset !important;">
     <form action="" method="post">
     <div class="section-header pt-2">
         <span class="text-muted pt-4" style="width: 95%;">Pending Time Change Requests</span>
