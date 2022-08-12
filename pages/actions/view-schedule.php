690a05f8250685c4edd3912cb57724116d64c223
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-12 15:00:19 -0600

    update

--- pages/actions/view-schedule.php
+++ pages/actions/view-schedule.php
@@ -33,7 +33,7 @@ if(!isLoggedIn()){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>WMS | View Job</title>
+   <title>WMS | View Schedule</title>
 
   <!-- START SCRIPTS -->
     <!-- Custom Styles -->
@@ -276,7 +276,7 @@ if(!isLoggedIn()){
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Schedule Change Request</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
