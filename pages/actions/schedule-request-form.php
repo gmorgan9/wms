40ef02590cae81d52a6cebbadad64d4b81026851
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-12 10:35:29 -0600

    update

--- pages/actions/schedule-request-form.php
+++ pages/actions/schedule-request-form.php
@@ -176,7 +176,7 @@ if(!isLoggedIn()){
     </div>
     <br>
     <hr>
-    <div class="section-header pt-2 text-center fs-6">
+    <div class="section-header pt-2 text-center fs-5">
       <span class="text-muted pt-4" style="width: 95%;">Schedule for</span>
     </div>
     <div class="row">
