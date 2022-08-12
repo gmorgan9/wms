69e7b461394f15c60f3db44a52193c85b1f941de
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-12 14:58:58 -0600

    update

--- pages/actions/view-schedule.php
+++ pages/actions/view-schedule.php
@@ -300,7 +300,7 @@ if(!isLoggedIn()){
                     <input class="form-control" id="reason" type="text" name="reason" value="<?php echo $row['idno'] ?>" readonly>
                 </div>
                 <div class="form-group pt-3 mx-auto">
-                    <label for="notes" style="font-size: 14px;">Notes</label>
+                    <label for="notes" style="font-size: 14px;">Reason <span class="text-muted" style="font-size: 10px;">List dates and times wanted to be changed. Why?</span></label>
                     <textarea class="form-control" id="reason" type="text" name="reason" value=""></textarea>
                 </div> <?php }} ?>
 
