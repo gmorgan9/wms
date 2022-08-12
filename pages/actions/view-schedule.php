611db309cbf6ad67a25c97fed7e3500a84c3938d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-12 14:26:18 -0600

    update

--- pages/actions/view-schedule.php
+++ pages/actions/view-schedule.php
@@ -197,10 +197,6 @@ if(!isLoggedIn()){
                         </div>
                       </div>
                       <hr>
-
-
-
-                      <hr>
                       <div class="row">
                         <div class="col-sm-3">
                           <h6 class="mb-0">Actions</h6>
