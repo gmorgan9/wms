90867d3ee01fcac618f0a7f9da60e2c79af9fe0f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-12 08:14:32 -0600

    update

--- pages/schedule_request.php
+++ pages/schedule_request.php
@@ -163,6 +163,7 @@ if(!isLoggedIn()){
 
   <!-- START JOB-REQUEST (RIGHT SIDE) -->
     <div class="page-content mt-2 mx-auto" style="margin-right: 10px;">
+    <a class="badge text-bg-secondary" href="schedule-request-form.php">Request Schedule</a>
     <table class="table">
     <thead>
       <tr>
