14775d89268f1a65c9bccb75c0643efea59ecf12
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-12 08:39:46 -0600

    update

--- pages/actions/schedule-request-form.php
+++ pages/actions/schedule-request-form.php
@@ -144,29 +144,29 @@ if(!isLoggedIn()){
     <div class="row mx-auto">
       <div class="form-group pt-3 mx-auto" style="width: 25%;">
         <label for="employee_fname" style="font-size: 14px;">First Name</label>
-        <input class="form-control" id="employee_fname" type="text" name="employee_fname" value="<?php echo $fname; ?>">
+        <input class="form-control" id="employee_fname" type="text" name="employee_fname" value="<?php echo $fname; ?>" disabled>
       </div>
       <div class="form-group pt-3 mx-auto" style="width: 25%;">
         <label for="employee_lname" style="font-size: 14px;">Last Name</label>
-        <input class="form-control" id="employee_lname" type="text" name="employee_lname" value="<?php echo $lname; ?>">
+        <input class="form-control" id="employee_lname" type="text" name="employee_lname" value="<?php echo $lname; ?>" disabled>
       </div>
       <div class="form-group pt-3 mx-auto" style="width: 15%;">
         <label for="employee_idno" style="font-size: 14px;">Employee ID #</label>
-        <input class="form-control" id="employee_idno" type="text" name="employee_idno" value="<?php echo $employeeID; ?>">
+        <input class="form-control" id="employee_idno" type="text" name="employee_idno" value="<?php echo $employeeID; ?>" disabled>
       </div>
       <div class="form-group pt-3 mx-auto" style="width: 35%;">
         <label for="jobtitle" style="font-size: 14px;">Job Title</label>
-        <input class="form-control" id="jobtitle" type="text" name="jobtitle" value="<?php echo $jobtitle; ?>">
+        <input class="form-control" id="jobtitle" type="text" name="jobtitle" value="<?php echo $jobtitle; ?>" disabled>
       </div>
     </div>
     <div class="row">
       <div class="form-group pt-3 mx-auto" style="width: 48%;">
         <label for="companyname" style="font-size: 14px;">Company</label>
-        <input class="form-control" id="companyname" type="text" name="companyname" value="<?php echo $companyname; ?>">
+        <input class="form-control" id="companyname" type="text" name="companyname" value="<?php echo $companyname; ?>" disabled>
       </div>
       <div class="form-group pt-3 mx-auto" style="width: 48%;">
         <label for="deptname" style="font-size: 14px;">Department</label>
-        <input class="form-control" id="deptname" type="text" name="deptname" value="<?php echo $deptname; ?>">
+        <input class="form-control" id="deptname" type="text" name="deptname" value="<?php echo $deptname; ?>" disabled>
       </div>
     </div>
     <br>
