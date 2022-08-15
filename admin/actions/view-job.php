e138cdd557c7b1d5518c08bad5878327bb22ac2b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-15 14:22:58 -0600

    update

--- admin/actions/view-job.php
+++ admin/actions/view-job.php
@@ -290,14 +290,25 @@ if(!isAdmin()){
           </div>
         <?php } ?>
       </div>
+      <?php if($row['status'] == 1) { ?>
       <div class="form-group pt-3">
         <label for="status" style="font-size: 14px;">Active Status <span class="text-muted" style="font-size: 10px;">e.g. "Active Job"</span></label>
         <select class="form-control" name="status" id="status">
           <option value="">Select One...</option>
-          <option value="1">Active Job</option>
+          <option value="1" selected>Active Job</option>
           <option value="0">Inactive Job</option>
         </select>
       </div>
+      <?php } else { ?>
+        <div class="form-group pt-3">
+        <label for="status" style="font-size: 14px;">Active Status <span class="text-muted" style="font-size: 10px;">e.g. "Active Job"</span></label>
+        <select class="form-control" name="status" id="status">
+          <option value="">Select One...</option>
+          <option value="1">Active Job</option>
+          <option value="0" selected>Inactive Job</option>
+        </select>
+      </div>
+      <?php } ?>
       <div class="form-group pt-3">
         <label for="note" style="font-size: 14px;">Notes</label>
         <textarea class="form-control" id="note" type="text" name="note"><?php echo $row['note']; ?></textarea>
