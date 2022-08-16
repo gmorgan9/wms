426fe527a0dec1d97ae0051aac0dbd319cab86a6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-16 16:34:00 -0600

    update

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -193,9 +193,9 @@ if(isset($_POST['create-timesheet'])) {
          <span class="pt-4" style="width: 95%;">Today's Date is <span class="text-muted"><?php echo $for_date; ?></span></span>
        </div>
        <!-- create-timesheet -->
-       <?php if($database_date == $date) {?>
+       <?php if($database_date == $date || $timein == null || $timeout == null) {?>
           <div class="section-header text-center pt-2">
-            <span class="text-muted pt-4" style="width: 95%; font-size: 14px;">Timesheet already created for today</span>
+            <span class="text-muted pt-4" style="width: 95%; font-size: 14px;">Timesheet created for today</span>
           </div>
         <?php } else { ?>
        <form method="post" action="">
