27785cf424b09bbb19ce4833ef83281dbf8f4365
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-16 16:55:08 -0600

    update

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -220,7 +220,7 @@ if(isset($_POST['create-timesheet'])) {
             <input type="hidden" name="employee_fname" value="<?php echo $emp_fname; ?>" />
             <input type="hidden" name="employee_lname" value="<?php echo $emp_lname; ?>" />
             <input type="hidden" name="date" value="<?php echo $date; ?>" />
-            <div class="mx-auto">
+            <div class="col text-center">
               <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="create-timesheet"><span class="badge text-bg-primary">Create Timesheet</span></button>
             </div>
           </form>
