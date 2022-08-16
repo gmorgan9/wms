e04b54a7f47e9dd1bab2bfa6302f57a92fd72d9a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-16 15:22:07 -0600

    update

--- pages/timeclock.php
+++ pages/timeclock.php
@@ -63,7 +63,7 @@ if (isset($_POST['timein'])) {
         $timezone = date_default_timezone_get();
         date_default_timezone_set($timezone);
         $date = date('Y-m-d');
-        $time = date('h:i a');
+        $time = date('h:i');
         echo $date;
         echo $time;
 
