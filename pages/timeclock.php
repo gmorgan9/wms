6adf30b01df3fd02036f13c832b08e06eb86c46a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-16 15:37:12 -0600

    update

--- pages/timeclock.php
+++ pages/timeclock.php
@@ -83,8 +83,8 @@ if (isset($_POST['clockout'])) {
         <form method="post" action="">
             <?php $empID = $_SESSION['employee_idno']; ?>
             <input type="text" name="employee_idno" value="<?php echo $empID; ?>" />
-            <input type="hidden" name="date" value="<?php echo $date; ?>" />
-            <input type="hidden" name="timein" value="<?php echo $time; ?>" />
+            <input type="date" name="date" value="<?php echo $date; ?>" />
+            <input type="time" name="timein" value="<?php echo $time; ?>" />
             <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="clockin"><span class="badge text-bg-success">Clock In</span></button>
         </form>
         <form method="post" action="">
