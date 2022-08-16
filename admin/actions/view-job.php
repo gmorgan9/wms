849551addd39ccbf526b9c80219033411be2f665
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-16 14:51:36 -0600

    update

--- admin/actions/view-job.php
+++ admin/actions/view-job.php
@@ -166,9 +166,9 @@ if (isset($_POST['inactive'])) {
                       ?>
                       <?php if($row['end_date'] == null && $row['start_date'] == null) { ?>
                         <span class="text-warning">Needs a End Date</span> - <span class="text-warning">Needs a End Date</span>
-                      <?php } if($row['end_date'] == null) { ?>
+                      <?php } else if($row['end_date'] == null) { ?>
                         <?php echo $end_date; ?> - <span class="text-warning">Needs a End Date</span>
-                      <?php } if($row['start_date'] == null) { ?>
+                      <?php } else if($row['start_date'] == null) { ?>
                         <span class="text-warning">Needs a Start Date</span> - <?php echo $end_date; ?>
                       <?php } else if ($row['end_date'] == null) { ?>
                         <?php echo $start_date; ?> - <span class="text-warning">Enter end date when job is terminated.</span>
