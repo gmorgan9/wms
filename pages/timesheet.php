669654a5fda724332a9db44f74de7cff3599f45c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-16 16:08:51 -0600

    update

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -148,8 +148,7 @@ if(!isLoggedIn()){
             $timeout = $row['timeout'];
          } }?>
         <div class="section-header text-center pt-2">
-          <?php $formatted_date = date('F d, Y', stripos($date)); ?>
-         <span class="pt-4" style="width: 95%;">Today's Date is <span class="text-muted"><?php echo $formatted_date ?></span></span>
+         <span class="pt-4" style="width: 95%;">Today's Date is <span class="text-muted"><?php echo date_format($date, 'F d, Y') ?></span></span>
        </div>
         
         <form id="clockin" method="post" action="">
