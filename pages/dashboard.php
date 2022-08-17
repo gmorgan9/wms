3116ebd7d89249c3838eafcc1b33a4584fee28d9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-17 14:35:01 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -57,7 +57,7 @@ if(!isLoggedIn()){
       <div class="row mt-3">
         <div class="col me-3 ms-3" style="height: 75px; background-color: #c9b8a9;">
           Time Card
-          <div class="col me-3 ms-3" style="margin-top: 65px; width: 85px !important; height: 75px; background-color: #c9b8a9;">
+          <div class="col" style="margin-top: 65px; width: 85px !important; height: 75px; background-color: #c9b8a9;">
           Welcome
         </div>
         </div>
