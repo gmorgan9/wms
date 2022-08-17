62a3a2b0ddfea570bd3e9c09fd3fc3233062c9c1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-17 14:13:43 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -55,9 +55,15 @@ if(!isLoggedIn()){
   <div class="page-content mt-2 mx-auto" style="margin-right: 10px;">
     <div class="container text-center">
       <div class="row">
-        <div class="col">col1</div>
-        <div class="col">col2</div>
-        <div class="col">col3</div>
+      <div class="col">
+      1 of 3
+    </div>
+    <div class="col">
+      2 of 3
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
       </div>
       <div class="row">
       <div class="col">
