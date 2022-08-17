d7d39e05e1307795280e9609149a7ff739252a16
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-17 14:23:23 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -54,7 +54,7 @@ if(!isLoggedIn()){
   <!-- NON ADMIN DASHBOARD -->
   <div class="page-content mt-2 mx-auto" style="margin-right: 10px;">
     <div class="container text-center">
-      <div class="row">
+      <div class="row mt-2">
       <div class="col me-3 ms-5" style="height: 350px; background-color: #000;">
       1 of 3
     </div>
@@ -66,10 +66,10 @@ if(!isLoggedIn()){
     </div>
       </div>
       <div class="row mt-2">
-      <div class="col me-3 ms-5" style="height: 250px; background-color: #000;">
+      <div class="col me-3 ms-5" style="height: 225px; background-color: #000;">
       1 of 2
     </div>
-    <div class="col me-5 ms-3" style="height: 250px; background-color: #000;">
+    <div class="col me-5 ms-3" style="height: 225px; background-color: #000;">
       2 of 2
     </div>
       </div>
