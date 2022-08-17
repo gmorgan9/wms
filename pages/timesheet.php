e473bee3362549bb71c5f0f7a0f7aba15f0bbe72
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-17 11:35:13 -0600

    update

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -321,12 +321,12 @@ if(!isLoggedIn()){
             <?php } else {}?> 
             <?php if ($row['timein'] == null) { ?>
               <div class="col text-center mt-3">
-              <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="clockin"><span class="badge text-bg-success">Clock In</span></button>
+              <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="create-timesheet"><span class="badge text-bg-primary">Create Timesheet</span></button>
             </div>
               <?php } else {}?>
               <?php if ($row['timeout'] == null) { ?>
                 <div class="col text-center mt-3">
-              <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="clockout"><span class="badge text-bg-primary">Clock Out</span></button>
+              <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="create-timesheet"><span class="badge text-bg-primary">Create Timesheet</span></button>
             </div>
             <?php }else {} ?>
           </form>
