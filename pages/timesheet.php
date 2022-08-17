52c588898a74eea1c173d46cec492a1ef463a94e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-17 11:53:54 -0600

    update

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -209,7 +209,9 @@ if(!isLoggedIn()){
                       $newtime = date("h:i:s",strtotime("+0 HOURS"));
                       $newdate = date("Y-m-d");
                       ?>
+                      <div class="section-header text-center pt-2">
                       <span class="pt-4" style="width: 95%;">Today's Date is <span id="tick2" class="timeh1"><?php echo $for_date; ?></span></span>
+        </div>
         </div>
 
             </center>
