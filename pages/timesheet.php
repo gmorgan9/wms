ba8f4dde47b25f2bb193672bdae51f7f384ceb8b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-17 08:11:34 -0600

    update

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -213,7 +213,7 @@ if(isset($_POST['create-timesheet'])) {
         function digitalClock(){
             servertime.setSeconds(servertime.getSeconds()+1);
             var timestring=padlength(servertime.getHours())+":"+padlength(servertime.getMinutes())+":"+padlength(servertime.getSeconds());
-            document.getElementById("clock").innerHTML="The Current Time is " + timestring + " MDT";
+            document.getElementById("clock").innerHTML="The Current Time is <span class='text-muted'>" + timestring + " MDT</span>";
         }
         window.onload=function(){
         setInterval("digitalClock()", 1000);
