d78f2708a9396ce928a89f895b6dfb6ac7e808a7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-17 10:57:51 -0600

    update

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -299,17 +299,31 @@ if(!isLoggedIn()){
             <input type="hidden" name="employee_fname" value="<?php echo $emp_fname; ?>" />
             <input type="hidden" name="employee_lname" value="<?php echo $emp_lname; ?>" />
             <input type="hidden" name="date" value="<?php echo $date; ?>" />
-            <?php if($date == null) { ?>
+
+
+            <?php 
+        $employee_idno = $_SESSION['employee_idno'];
+        $select = " SELECT * FROM timesheet WHERE date = '$date' ";
+        $result = mysqli_query($conn, $select);
+
+        if (mysqli_num_rows($result) > 0) {
+         while($row = mysqli_fetch_assoc($result)) {
+            $database_date = $row['date'];
+            $database_timein = $row['timein'];
+            $database_timeout = $row['timeout'];
+         } }?>
+
+            <?php if($database_date == null) { ?>
             <div class="col text-center mt-3">
               <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="create-timesheet"><span class="badge text-bg-primary">Create Timesheet</span></button>
             </div>
-            <?php } else if ($timein == null) { ?>
+            <?php } else if ($database_timein == null) { ?>
               <div class="col text-center mt-3">
-              <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="create-timesheet"><span class="badge text-bg-primary">Clock In</span></button>
+              <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="clockin"><span class="badge text-bg-primary">Clock In</span></button>
             </div>
-              <?php } else if ($timeout == null) { ?>
+              <?php } else if ($database_timeout == null) { ?>
                 <div class="col text-center mt-3">
-              <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="create-timesheet"><span class="badge text-bg-primary">Create Timesheet</span></button>
+              <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="clockout"><span class="badge text-bg-primary">Clock Out</span></button>
             </div>
             <?php } ?>
           </form>
