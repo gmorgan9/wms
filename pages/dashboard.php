252666d0b6b6ee43635bd29842c42a336c6724f8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-17 14:25:30 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -61,7 +61,7 @@ if(!isLoggedIn()){
     <div class="col me-3 ms-3" style="height: 350px; background-color: #c9b8a9;">
       2 of 3
     </div>
-    <div class="col me-5 ms-3" style="height: 350px; background-color: #c9b8a9;">
+    <div class="col me-3 ms-3" style="height: 350px; background-color: #c9b8a9;">
       3 of 3
     </div>
       </div>
@@ -69,7 +69,7 @@ if(!isLoggedIn()){
       <div class="col me-3 ms-3" style="height: 230px; background-color: #c9b8a9;">
       1 of 2
     </div>
-    <div class="col me-5 ms-3" style="height: 230px; background-color: #c9b8a9;">
+    <div class="col me-3 ms-3" style="height: 230px; background-color: #c9b8a9;">
       2 of 2
     </div>
       </div>
