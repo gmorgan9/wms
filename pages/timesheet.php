9ae476c43aceb645f9da78d78d2306d657ea7346
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-17 11:04:06 -0600

    udpate

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -314,11 +314,11 @@ if(!isLoggedIn()){
             $database_timeout = $row['timeout'];
          } }?>
 
-            <?php if($database_date == $date && $timein == null && $timeout == null) { ?>
+            <?php if($database_date == $date && $database_timein == null && $database_timeout == null) { ?>
             <div class="col text-center mt-3">
               <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="create-timesheet"><span class="badge text-bg-primary">Create Timesheet</span></button>
             </div>
-            <?php } else if ($database_date == $date && $timeout == null) { ?>
+            <?php } else if ($database_date == $date && $database_timeout == null) { ?>
               <div class="col text-center mt-3">
               <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="clockin"><span class="badge text-bg-primary">Clock In</span></button>
             </div>
