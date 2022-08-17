ad04136b09d479452a6cd6592daa90365c4f729a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-17 08:40:56 -0600

    update

--- pages/timesheet.php
+++ pages/timesheet.php
@@ -212,7 +212,7 @@ if(isset($_POST['create-timesheet'])) {
     
     function startTime()
     {
-        document.getElementById('time').innerHTML = Date();  
+        document.getElementById('time').innerHTML = Date(HH:mm:ss);  
     }
   </script>
        
