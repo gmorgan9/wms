99334243dd3529c724f7978df54420f1fff5effa
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 08:20:31 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -75,7 +75,7 @@ if(!isLoggedIn()){
       <div class="row mt-3">
         <div class="col me-3 ms-3" style="height: 75px; background-color: #eee; border-radius: 15px;">
           <div class="col-content" style="padding-top: 15px;">
-            <span class="">Welcome, <?php echo $fname; ?>!</span>
+            <span class="">Welcome Back, <?php echo $fname; ?>!</span>
           </div>
           <div class="col" style="margin-top: 50px; height: 130px; background-color: #eee; border-radius: 15px;">
             Time Card
