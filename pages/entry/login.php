60b15ebcb62688c6ede1cfa9124ace4b273a4e56
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 13:07:40 -0600

    update

--- pages/entry/login.php
+++ pages/entry/login.php
@@ -13,48 +13,7 @@ if(isLoggedIn()){
 
 
 
-if(isset($_POST['submit'])){
 
-   $empID = mysqli_real_escape_string($conn, $_POST['employeeID']);
-   $fname = mysqli_real_escape_string($conn, $_POST['fname']);
-   $lname = mysqli_real_escape_string($conn, $_POST['lname']);
-   $uname = mysqli_real_escape_string($conn, $_POST['uname']);
-   $email = mysqli_real_escape_string($conn, $_POST['email']);
-   $pass = md5($_POST['password']);
-   $cpass = md5($_POST['cpassword']);
-   $isadmin = $_POST['isadmin'];
-   $loggedin = $_POST['loggedin'];
-
-   $select = " SELECT * FROM employee WHERE uname = '$uname' && password = '$pass' ";
-
-   $result = mysqli_query($conn, $select);
-
-   if(mysqli_num_rows($result) > 0){
-
-      $row = mysqli_fetch_array($result);
-      $sql = "UPDATE employee SET loggedin='1' WHERE uname='$uname'";
-      if (mysqli_query($conn, $sql)) {
-         echo "Record updated successfully";
-       } else {
-         echo "Error updating record: " . mysqli_error($conn);
-       }
-       $_SESSION['fname']           = $row['fname'];
-       $_SESSION['empID']           = $row['employeeID'];
-       $_SESSION['loggedin']        = $row['loggedin'];
-       $_SESSION['employee_idno']   = $row['idno'];
-       $_SESSION['lname']           = $row['lname'];
-       $_SESSION['acc_type']        = $row['acc_type'];
-       $_SESSION['uname']           = $row['uname'];
-      $_SESSION['email']            = $row['email'];
-      $_SESSION['pass']             = $row['password'];
-      $_SESSION['cpass']            = $row['cpassword'];
-      header('location:' . BASE_URL . '/pages/dashboard.php');
-     
-   }else{
-      $error[] = 'incorrect email or password!';
-   }
-
-};
 ?>
 
 <!DOCTYPE html>
@@ -92,7 +51,7 @@ if(isset($_POST['submit'])){
       ?>
       <input type="text" name="uname" required placeholder="enter your user name">
       <input type="password" name="password" required placeholder="enter your password">
-      <input type="submit" name="submit" value="Login" class="form-btn">
+      <input type="submit" name="login-btn" value="Login" class="form-btn">
       <p>don't have an account? <a href="/pages/entry/register.php">register now</a></p>
    </form>
 
