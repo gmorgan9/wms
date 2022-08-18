5d4442c05ce2159f38e79e08b6ea760bcc74cdf1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 15:56:48 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -524,6 +524,21 @@ if(!isLoggedIn()){
                     opacity: 0.8;
                 }
               </style>
+              <script>
+                setInterval(() => {
+                    d = new Date(); //object of date()
+                    hr = d.getHours();
+                    min = d.getMinutes();
+                    sec = d.getSeconds();
+                    hr_rotation = 30 * hr + min / 2; //converting current time
+                    min_rotation = 6 * min;
+                    sec_rotation = 6 * sec;
+                                
+                    hour.style.transform = `rotate(${hr_rotation}deg)`;
+                    minute.style.transform = `rotate(${min_rotation}deg)`;
+                    second.style.transform = `rotate(${sec_rotation}deg)`;
+                }, 1000);
+              </script>
               <div id="clockContainer">
                 <div id="hour"></div>
                 <div id="minute"></div>
