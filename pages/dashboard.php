bfa6efb4117a9c973d7f4c7ca9e7c4dab53cec37
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 17:35:26 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -605,33 +605,22 @@ if(!isLoggedIn()){
                       ?>
                       <span class="text-center">
                         <?php echo $cdt_current_date; ?>
-                        <script>
-    function currentTime() {
-  let date = new Date(); 
-  let hh = date.getHours();
-  let mm = date.getMinutes();
-  let ss = date.getSeconds();
-  let session = "AM";
-
-  if(hh === 0){
-      hh = 12;
-  }
-  if(hh > 12){
-      hh = hh - 12;
-      session = "PM";
-   }
-
-   hh = (hh < 10) ? "0" + hh : hh;
-   mm = (mm < 10) ? "0" + mm : mm;
-   ss = (ss < 10) ? "0" + ss : ss;
-    
-   let time = hh + ":" + mm + ":" + ss + " " + session;
-
-  document.getElementById("clock").innerText = time; 
-  let t = setTimeout(function(){ currentTime() }, 1000);
+                        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+ 
+<div id="runningTime"></div>
+ 
+<script type="text/javascript">
+$(document).ready(function() {
+ setInterval(runningTime, 1000);
+});
+function runningTime() {
+  $.ajax({
+    url: 'timeScript.php',
+    success: function(data) {
+       $('#runningTime').html(data);
+     },
+  });
 }
-
-currentTime();
 </script>
                       </span>
             </div>
