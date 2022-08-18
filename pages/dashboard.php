31ab46cdfb2f9437f29a4586d82655da9ea8b1b4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 08:04:53 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -89,6 +89,15 @@ if(!isLoggedIn()){
                   </div>
                 </div>
               </div>
+              <div class="col-md-8 float-start ms-4" style="margin-top: 33px; width: 165px;">
+                <div class="card mb-3" style="border-color: white;">
+                <div class="card-body">
+                  <h3>
+                    <?php echo $fname; ?> <?php echo $lname; ?>
+                  </h3>
+                  </div>
+                </div>
+              </div>
         </div>
       </div>
       <div class="row mt-3">
