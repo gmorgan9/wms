febc42a4e51eab2c64df6f8c256fcb4c93e8b14b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 12:48:44 -0600

    update

--- pages/entry/login.php
+++ pages/entry/login.php
@@ -1,60 +1,61 @@
 <!-- WORKING -->
+<?php include('../../app/database/functions.php"') ?>
 <?php
 
 require_once "../../app/database/connection.php";
-require_once "../../app/database/functions.php";
+//require_once "../../app/database/functions.php";
 require_once "../../path.php";
 
 session_start();
 
-if(isLoggedIn()){
-   header('location:' . BASE_URL . '/pages/dashboard.php');
-}
+// if(isLoggedIn()){
+//    header('location:' . BASE_URL . '/pages/dashboard.php');
+// }
 
 
 
-if(isset($_POST['login'])){
+// if(isset($_POST['login'])){
 
-   //$empID = mysqli_real_escape_string($conn, $_POST['employeeID']);
-   //$fname = mysqli_real_escape_string($conn, $_POST['fname']);
-   //$lname = mysqli_real_escape_string($conn, $_POST['lname']);
-   $uname = mysqli_real_escape_string($conn, $_POST['uname']);
-   //$email = mysqli_real_escape_string($conn, $_POST['email']);
-   $pass = md5($_POST['password']);
-   $cpass = md5($_POST['cpassword']);
-   //$isadmin = $_POST['isadmin'];
-   $loggedin = $_POST['loggedin'];
+//    //$empID = mysqli_real_escape_string($conn, $_POST['employeeID']);
+//    //$fname = mysqli_real_escape_string($conn, $_POST['fname']);
+//    //$lname = mysqli_real_escape_string($conn, $_POST['lname']);
+//    $uname = mysqli_real_escape_string($conn, $_POST['uname']);
+//    //$email = mysqli_real_escape_string($conn, $_POST['email']);
+//    $pass = md5($_POST['password']);
+//    $cpass = md5($_POST['cpassword']);
+//    //$isadmin = $_POST['isadmin'];
+//    $loggedin = $_POST['loggedin'];
 
-   $select = " SELECT * FROM employee WHERE uname = '$uname' && password = '$pass' ";
+//    $select = " SELECT * FROM employee WHERE uname = '$uname' && password = '$pass' ";
 
-   $result = mysqli_query($conn, $select);
+//    $result = mysqli_query($conn, $select);
 
-   if(mysqli_num_rows($result) > 0){
+//    if(mysqli_num_rows($result) > 0){
 
-      $row = mysqli_fetch_array($result);
-      $sql = "UPDATE employee SET loggedin='1' WHERE uname='$uname'";
-      if (mysqli_query($conn, $sql)) {
-         echo "Record updated successfully";
-       } else {
-         echo "Error updating record: " . mysqli_error($conn);
-       }
-       $_SESSION['fname']           = $row['fname'];
-       $_SESSION['empID']           = $row['employeeID'];
-       $_SESSION['loggedin']        = $row['loggedin'];
-       $_SESSION['employee_idno']   = $row['idno'];
-       $_SESSION['lname']           = $row['lname'];
-       $_SESSION['acc_type']        = $row['acc_type'];
-       $_SESSION['uname']           = $row['uname'];
-      $_SESSION['email']            = $row['email'];
-      $_SESSION['pass']             = $row['password'];
-      $_SESSION['cpass']            = $row['cpassword'];
-      header('location:' . BASE_URL . '/pages/dashboard.php');
+//       $row = mysqli_fetch_array($result);
+//       $sql = "UPDATE employee SET loggedin='1' WHERE uname='$uname'";
+//       if (mysqli_query($conn, $sql)) {
+//          echo "Record updated successfully";
+//        } else {
+//          echo "Error updating record: " . mysqli_error($conn);
+//        }
+//        $_SESSION['fname']           = $row['fname'];
+//        $_SESSION['empID']           = $row['employeeID'];
+//        $_SESSION['loggedin']        = $row['loggedin'];
+//        $_SESSION['employee_idno']   = $row['idno'];
+//        $_SESSION['lname']           = $row['lname'];
+//        $_SESSION['acc_type']        = $row['acc_type'];
+//        $_SESSION['uname']           = $row['uname'];
+//       $_SESSION['email']            = $row['email'];
+//       $_SESSION['pass']             = $row['password'];
+//       $_SESSION['cpass']            = $row['cpassword'];
+//       header('location:' . BASE_URL . '/pages/dashboard.php');
      
-   }else{
-      $error[] = 'incorrect email or password!';
-   }
+//    }else{
+//       $error[] = 'incorrect email or password!';
+//    }
 
-};
+// };
 ?>
 
 <!DOCTYPE html>
