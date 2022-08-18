b2ef0b7c1dcd29c040e04f7c1465eec833129063
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 07:44:32 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -75,11 +75,11 @@ if(!isLoggedIn()){
       <div class="row mt-3">
         <div class="col me-3 ms-3" style="height: 75px; background-color: #c9b8a9;">
           <div class="col-content" style="padding-top: 15px;">
-            <span class="">Welcome, <?php echo $fname; ?>!</span> <br>
+            <span class="">Welcome, <?php echo $fname; ?>!</span> <br><br>
             <?php if($email == null) { ?>
               <span class="text-danger">Your account isn't complete</span>
             <?php } else { ?>
-              <span class="text-success">Your account is compliant</span>
+              <span class="alert alert-primary" role="alert">Your account is compliant</span>
             <?php } ?>  
           </div>
           <div class="col" style="margin-top: 25px; height: 130px; background-color: #c9b8a9;">
