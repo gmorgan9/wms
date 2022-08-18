0a391bf4189f4b046c70e1bb02fd80f4175dc721
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 12:29:13 -0600

    update

--- pages/entry/login.php
+++ pages/entry/login.php
@@ -69,6 +69,9 @@ if(isset($_POST['submit'])){
 <!-- Bootstrap Icons -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
+<!-- scripts -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
 <body>
 
