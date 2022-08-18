8e29bc629f825b6af03c23d52fba17ddf067385a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 14:47:46 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -210,7 +210,7 @@ if(!isLoggedIn()){
               <span class="text-muted" style="font-size: 13px;">
                 <?php echo $f_curr_mon; ?> - <?php echo $f_curr_fri; ?>
               </span>
-              <table class="table mt-3">
+              <table class="table mt-1">
               <thead>
                 <tr>
                 </tr>
