e6c912d1b20b8d0401b8129a9eb459c1d97cc8a1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 14:22:28 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -187,7 +187,7 @@ if(!isLoggedIn()){
                     <?php echo $short_f_mon; ?>
                   </th>
                   <td class="text-start">
-                    <span class="lh-1">
+                    <span class="lh-base">
                       <?php echo $f_mon_timein; ?> - <?php echo $f_mon_timeout; ?>
                     </span>
                   </td>
