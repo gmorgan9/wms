9f4006e7218707d4085b4d710a815bc73f24ac3b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 08:52:41 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -74,10 +74,11 @@ if(!isLoggedIn()){
     <div class="container text-center">
       <div class="row mt-3">
         <div class="col me-3 ms-3" style="height: 75px; background-color: #eee; border-radius: 15px;">
-          <div class="col-content" style="padding-top: 18px;">
+          <div class="col-content" style="padding-top: 15px;">
             <h2>Welcome Back, <?php echo $fname; ?>!</h2>
+            <span class="text-muted">Check your notifications and messages.</span>
           </div>
-          <div class="col" style="margin-top: 50px; height: 130px; background-color: #eee; border-radius: 15px;">
+          <div class="col" style="margin-top: 35px; height: 130px; background-color: #eee; border-radius: 15px;">
             <div class="col-content" style="padding-top: 10px;">
               <h5>
                 Time Card
