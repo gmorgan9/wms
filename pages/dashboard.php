983928663d1cb42dfa28ba739c78aa3168b546f7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 16:27:11 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -552,7 +552,7 @@ if(!isLoggedIn()){
                   position: absolute;
                   width: 4px;
                   height: 90px;
-                  background: rgb(102, 109, 115);
+                  background: rgb(120, 120, 120);
                   z-index: 11;
                   border-radius: 6px 6px 0 0;
                   }
@@ -562,7 +562,7 @@ if(!isLoggedIn()){
                   position: absolute;
                   width: 2px;
                   height: 90px;
-                  background: rgb(102, 109, 115);
+                  background: rgb(120, 120, 120);
                   z-index: 12;
                   border-radius: 6px 6px 0 0;
                   }
