af03e79d881074bc78154d89643a48e4f13ac66f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 17:41:19 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -606,16 +606,49 @@ if(!isLoggedIn()){
                       
                       <span class="text-center">
                         <?php echo $cdt_current_date; ?>
-                        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script type="text/javascript">
-var timestamp = '<?=time();?>';
-function updateTime(){
-  $('#time').html(Date(timestamp));
-  timestamp++;
-}
-$(function(){
-  setInterval(updateTime, 1000);
-});
+                        <script>
+		    flag = true;
+			timer = '';
+			setInterval(function(){phpJavascriptClock();},1000);
+
+			function phpJavascriptClock()
+			{
+				if ( flag ) {
+					timer = <?php echo $current_timestamp;?>*1000;
+				}
+				var d = new Date(timer);
+				months = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec');
+
+				month_array = new Array('January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'Augest', 'September', 'October', 'November', 'December');
+
+				currentYear = d.getFullYear();
+				month = d.getMonth();
+				var currentMonth = months[month];
+				var currentMonth1 = month_array[month];
+				var currentDate = d.getDate();
+				currentDate = currentDate < 10 ? '0'+currentDate : currentDate;
+
+				var hours = d.getHours();
+				var minutes = d.getMinutes();
+				var seconds = d.getSeconds();
+
+				var ampm = hours >= 12 ? 'PM' : 'AM';
+				hours = hours % 12;
+				hours = hours ? hours : 12; // the hour ’0' should be ’12'
+				minutes = minutes < 10 ? '0'+minutes : minutes;
+				seconds = seconds < 10 ? '0'+seconds : seconds;
+				var strTime = hours + ':' + minutes+ ':' + seconds + ' ' + ampm;
+				document.getElementById("demo").innerHTML= currentMonth+' ' + currentDate+' , ' + currentYear + ' ' + strTime ;
+
+				document.getElementById("demo1").innerHTML= currentMonth1+' ' + currentDate+' , ' + currentYear + ' ' + strTime ;
+
+				document.getElementById("demo2").innerHTML= currentDate+':' +month+':' +currentYear + ' ' + strTime ;
+
+				document.getElementById("demo3").innerHTML= strTime ;
+
+				flag = false;
+				timer = timer + 1000;
+			}
 </script>
                       </span>
             </div>
