ab4523b9b0828eb26e75c1ad05ae989bcb897d52
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 07:46:35 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -82,7 +82,7 @@ if(!isLoggedIn()){
               <span class="alert alert-primary" role="alert">Your account is compliant</span>
             <?php } ?>  
           </div>
-          <div class="col" style="margin-top: 40px; height: 85px; background-color: #c9b8a9;">
+          <div class="col" style="margin-top: 40px; height: 90px; background-color: #c9b8a9;">
             Time Card
           </div>
         </div>
