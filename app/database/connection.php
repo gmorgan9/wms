d7368ccca02b7fe5554e1dbb479dca8b13d7816c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 12:54:08 -0600

    update

--- app/database/connection.php
+++ app/database/connection.php
@@ -14,5 +14,7 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
+} else {
+  echo "success";
 }
 ?>
