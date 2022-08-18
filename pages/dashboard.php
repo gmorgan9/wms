b883ac8854580374992b96d7cff3e8c439068f64
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 16:16:50 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -498,74 +498,74 @@ if(!isLoggedIn()){
                   inset 0 15px 15px rgba(0,0,0,0.3);
                   }
                   .clock::before
-{
-content: '';
-position: absolute;
-width: 15px;
-height: 15px;
-background: #fff;
-border-radius: 50%;
-z-index: 10000;
-}
-.clock .hour,
-.clock .min,
-.clock .sec
-{
-position: absolute;
-}
-.clock .hour, .hr
-{
-width: 160px;
-height: 160px;
-}
-.clock .min, .mn
-{
-width: 190px;
-height: 190px;
-}
-.clock .sec, .sc
-{
-width: 230px;
-height: 230px;
-}
-.hr, .mn, .sc
-{
-display: flex;
-justify-content: center;
-/* align-items: center; */
-position: absolute;
-border-radius: 50%;
-}
-.hr:before
-{
-content: '';
-position: absolute;
-width: 8px;
-height: 80px;
-background: #ff105e;
-z-index: 10;
-border-radius: 6px 6px 0 0;
-}
-.mn:before
-{
-content: '';
-position: absolute;
-width: 4px;
-height: 90px;
-background: #fff;
-z-index: 11;
-border-radius: 6px 6px 0 0;
-}
-.sc:before
-{
-content: '';
-position: absolute;
-width: 2px;
-height: 150px;
-background: #fff;
-z-index: 12;
-border-radius: 6px 6px 0 0;
-}
+                  {
+                  content: '';
+                  position: absolute;
+                  width: 15px;
+                  height: 15px;
+                  background: #fff;
+                  border-radius: 50%;
+                  z-index: 10000;
+                  }
+                  .clock .hour,
+                  .clock .min,
+                  .clock .sec
+                  {
+                  position: absolute;
+                  }
+                  .clock .hour, .hr
+                  {
+                  width: 160px;
+                  height: 160px;
+                  }
+                  .clock .min, .mn
+                  {
+                  width: 190px;
+                  height: 190px;
+                  }
+                  .clock .sec, .sc
+                  {
+                  width: 230px;
+                  height: 230px;
+                  }
+                  .hr, .mn, .sc
+                  {
+                  display: flex;
+                  justify-content: center;
+                  /* align-items: center; */
+                  position: absolute;
+                  border-radius: 50%;
+                  }
+                  .hr:before
+                  {
+                  content: '';
+                  position: absolute;
+                  width: 8px;
+                  height: 80px;
+                  background: #ff105e;
+                  z-index: 10;
+                  border-radius: 6px 6px 0 0;
+                  }
+                  .mn:before
+                  {
+                  content: '';
+                  position: absolute;
+                  width: 4px;
+                  height: 90px;
+                  background: #fff;
+                  z-index: 11;
+                  border-radius: 6px 6px 0 0;
+                  }
+                  .sc:before
+                  {
+                  content: '';
+                  position: absolute;
+                  width: 2px;
+                  height: 150px;
+                  background: #fff;
+                  z-index: 12;
+                  border-radius: 6px 6px 0 0;
+                  }
                 
               </style>
               <script>
@@ -582,6 +582,23 @@ border-radius: 6px 6px 0 0;
                       <div class="sc" id="sc"></div>
                       </div>
                       </div>
+                      <!-- Start script part -->
+                      <script type="text/Javascript">
+                      const deg = 6;
+                      const hr = document.querySelector('#hr');
+                      const mn = document.querySelector('#mn');
+                      const sc = document.querySelector('#sc');
+                      setInterval(() => {
+                      let day = new Date();
+                      let hh = day.getHours() * 30;
+                      let mm = day.getMinutes() * deg;
+                      let ss = day.getSeconds() * deg;
+                      hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+                      mn.style.transform = `rotateZ(${mm}deg)`;
+                      sc.style.transform = `rotateZ(${ss}deg)`;
+                      })
+                      </script>
+                      <!-- End script part -->
             </div>
         </div>
       </div>
