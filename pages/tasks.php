a7d92f45d8dcd10c67020451c03fdc86a6b98d28
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-19 11:54:23 -0600

    update

--- pages/tasks.php
+++ pages/tasks.php
@@ -194,12 +194,7 @@ if(!isLoggedIn()){
           <!-- <td><?php //echo $companyname; ?></td> -->
           <td>
             <div class="d-flex">
-                
-                <form method="post" action="" id="myform">
-                  <input type="hidden" id="newid" value="<?php echo $row['taskID']; ?>" />
-
-                  <button type="submit"><span class="viewbtn">Edit</span></button>
-                </form>
+            <button data-bs-toggle="modal" data-bs-target="#edit_modal_<?php echo $taskID; ?>" style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;"><span class="badge text-bg-danger">Delete</span></button>
                 <form method="post" action="">
                   <input type="hidden" name="taskID" value="<?php echo $taskID; ?>" />
 
@@ -226,7 +221,24 @@ if(!isLoggedIn()){
 
 
 <!-- MODAL -->
-    
+    <!-- Modal -->
+    <div class="modal fade" id="edit_modal_<?php echo $taskID; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
 <!-- END MODAL -->
 
 <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
