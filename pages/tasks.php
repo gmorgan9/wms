cae4f2f06060b007987d8f7a2e0e2c2b763c6ff3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-19 11:08:00 -0600

    update

--- pages/tasks.php
+++ pages/tasks.php
@@ -270,7 +270,7 @@ if(!isLoggedIn()){
 <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
 
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 
