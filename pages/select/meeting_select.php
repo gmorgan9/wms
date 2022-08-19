5d490fedb5b150b83afcf9645b9d768f953806c5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-19 14:28:13 -0600

    update

--- pages/select/meeting_select.php
+++ pages/select/meeting_select.php
@@ -57,7 +57,7 @@ if(isset($_POST["meeting_id"]))
                 <hr>
                 <div class="row">
                     <div class="col-sm-3">
-                      <h6 class="mb-0">Meeting Link</h6>
+                      <h6 class="mb-0">Link</h6>
                     </div>
                     <div class="col-sm-9">
                         '.$row["link"].'
