e5debed2ff0a7af3cc38cd692ccf47b1e7b25404
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-19 07:39:26 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -339,7 +339,7 @@ if(!isLoggedIn()){
                       &nbsp; Mon <br>
                       <?php echo $s_f_mon; ?>
                     </th>
-                    <?php if ($f_mon_timein != null) { ?>
+                    <?php if ($db_mon_timein != null) { ?>
                     <td class="text-start text-bg-secondary" style="width: 65%;">
                       <div style="opacity:0; font-size: 5px;">test</div>
                       <?php echo $f_mon_timein; ?> - <?php echo $f_mon_timeout; ?>
@@ -357,7 +357,7 @@ if(!isLoggedIn()){
                       &nbsp; Mon <br>
                       <?php echo $s_f_mon; ?>
                     </th>
-                    <?php if ($f_mon_timein != null) { ?>
+                    <?php if ($db_mon_timein != null) { ?>
                     <td class="text-start" style="width: 65%;">
                       <div style="opacity:0; font-size: 5px;">test</div>
                       <?php echo $f_mon_timein; ?> - <?php echo $f_mon_timeout; ?>
