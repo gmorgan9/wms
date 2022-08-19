e39b224f386b0d74694c0f20fe7d0378a1bb3508
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 18:39:59 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -406,7 +406,7 @@ if(!isLoggedIn()){
                 </tr>
                 <?php } else { ?>
                 <tr>
-                <th scope="row" style="font-size: 12px;">
+                <th scope="row" style="font-size: 12px;" style="width: 25%">
                     &nbsp; Thurs <br>
                     <?php echo $s_f_thurs; ?>
                   </th>
