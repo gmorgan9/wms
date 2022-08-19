a022a4b8622d138c17b82c37715c87da56716e74
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-19 10:59:23 -0600

    update

--- pages/tasks.php
+++ pages/tasks.php
@@ -191,7 +191,7 @@ if(!isLoggedIn()){
           <?php }?>
           <!-- <td><?php //echo $companyname; ?></td> -->
           <td>
-          <button type="button" class="btn btn-success editbtn"> EDIT </button>
+          <button type="button" data-bs-toggle="modal" data-bs-target="#editmodal">Launch modal</button>
           <!-- <button  style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit"><span class="badge text-bg-success editbtn">Edit</span></button> -->
             <form method="post" action="">
               <input type="hidden" name="jobID" value="<?php echo $jobID; ?>" />
@@ -271,12 +271,6 @@ if(!isLoggedIn()){
 
 <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script> -->
-    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script> -->
-
-    <!-- <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script> -->
-    <!-- <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script> -->
 
 
 
