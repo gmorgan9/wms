ff25af85ecb7fd8c5cc508e7ee3e690ab9f836c3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 18:13:42 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -599,7 +599,7 @@ if(!isLoggedIn()){
                       hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
                       mn.style.transform = `rotateZ(${mm}deg)`;
                       sc.style.transform = `rotateZ(${ss}deg)`;
-                      document.getElementById("time").innerHTML=('0' + hour.slice(-2))+":"+min+":"+sec;
+                      document.getElementById("time").innerHTML=hour+":"+min+":"+sec;
                       })
                       </script>
                       <!-- End script part -->
