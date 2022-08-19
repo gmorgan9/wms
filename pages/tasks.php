e39d6e9a893ffd0323480d7fe5f3990e1236195b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-19 12:17:49 -0600

    update

--- pages/tasks.php
+++ pages/tasks.php
@@ -194,7 +194,7 @@ if(!isLoggedIn()){
           <!-- <td><?php //echo $companyname; ?></td> -->
           <td>
             <div class="d-flex">
-            <button data-role="update" data-bs-toggle="modal" data-bs-target="#myModal" data-id="<?php echo $row['taskID'] ;?>" style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;"><span class="badge text-bg-success">Edit</span></button>
+            <button type="button" data-toggle="modal" data-target="#deletemodale<?php echo $row['taskID'];?>" >Open Modal</button>
                 <form method="post" action="">
                   <input type="hidden" name="taskID" value="<?php echo $taskID; ?>" />
 
@@ -221,26 +221,25 @@ if(!isLoggedIn()){
 
 
 <!-- MODAL -->
-    <!-- Modal -->
-    <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-          <input type="hidden" id="id" class="form-control">
-            <input type="text" id="title" class="form-control">
+<div class="modal fade" id="deletemodale<?php echo $row['taskID'];?>" role="dialog">
+    <div class="modal-dialog">
 
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
+      <!-- Modal content-->
+      <div class="modal-content" >
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
         </div>
       </div>
+
     </div>
+  </div>
 <!-- END MODAL -->
 
 <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
@@ -258,25 +257,6 @@ if(!isLoggedIn()){
 <script type="text/javascript" src="/mdb_gallery/public/js/mdb.min.js"></script>
 
 
-
-<script>
-      $(document).ready(function(){
-
-        //  append values in input fields
-          $(document).on('click','a[data-role=update]',function(){
-                var id  = $(this).data('id');
-                var title  = $('#'+id).children('td[data-target=title]').text();
-                //var lastName  = $('#'+id).children('td[data-target=lastName]').text();
-                //var email  = $('#'+id).children('td[data-target=email]').text();
-
-                //$('#firstName').val(firstName);
-                //$('#lastName').val(lastName);
-                $('#title').val(title);
-                $('#id').val(id);
-                $('#myModal').modal('toggle');
-          });
-        });
- </script>
 
 </body>
 </html>
