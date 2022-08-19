d7dd522c80f190daad41dac85e72c27e29aef68d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-19 12:52:16 -0600

    update

--- pages/select.php
+++ pages/select.php
@@ -16,51 +16,47 @@ if(isset($_POST["task_id"]))
     while($row = mysqli_fetch_array($result))  
     {  
          $output .= ' 
-
-                    <div class="col-md-8 float-end me-4">
-                        <div class="card mb-3">
-                            <div class="card-body">
-
-                                <div class="row">
-                                    <div class="col-sm-3">
-                                      <h6 class="mb-0">Title</h6>
-                                    </div>
-                                    <div class="col-sm-9">
-                                        '.$row["title"].'
-                                    </div>
-                                </div>
-                                <hr>
-                                <div class="row">
-                                    <div class="col-sm-3">
-                                      <h6 class="mb-0">Category</h6>
-                                    </div>
-                                    <div class="col-sm-9">
-                                        '.$row["category"].'
-                                    </div>
-                                </div>
-                                <hr>
-                                <div class="row">
-                                    <div class="col-sm-3">
-                                      <h6 class="mb-0">Details</h6>
-                                    </div>
-                                    <div class="col-sm-9">
-                                        '.$row["details"].'
-                                    </div>
-                                </div>
-                                <hr>
-                                <div class="row">
-                                    <div class="col-sm-3">
-                                      <h6 class="mb-0">Due Date</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-info">
-                                        '.$row["due_date"].'
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+    <div class="col-md-8 me-4">
+        <div class="card mb-3">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Title</h6>
                     </div>
-            
-                    ';  
+                    <div class="col-sm-9">
+                        '.$row["title"].'
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Category</h6>
+                    </div>
+                    <div class="col-sm-9">
+                        '.$row["category"].'
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Details</h6>
+                    </div>
+                    <div class="col-sm-9">
+                        '.$row["details"].'
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Due Date</h6>
+                    </div>
+                    <div class="col-sm-9">
+                        '.$row["due_date"].'
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>';  
     }  
     $output .= "</table></div>";  
     echo $output;  
