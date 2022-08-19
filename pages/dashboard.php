bae8d56138aa74eb9a701350cf3734f44e1b40e6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-18 18:04:33 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -594,7 +594,7 @@ if(!isLoggedIn()){
                       let mm = day.getMinutes() * deg;
                       let ss = day.getSeconds() * deg;
                       var hour =day.getHours() % 12;
-                      var min = day.getMinutes();
+                      var min = day.getMinutes() % 60;
 	                    var sec = day.getSeconds();
                       hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
                       mn.style.transform = `rotateZ(${mm}deg)`;
