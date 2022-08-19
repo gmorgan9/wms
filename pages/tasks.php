42021bd62dda5353e389e6bba2ed234cb0a41f9f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-19 10:12:41 -0600

    update

--- pages/tasks.php
+++ pages/tasks.php
@@ -124,9 +124,9 @@ if(!isLoggedIn()){
     $lname = $row['lname']; 
     $employeeID = $row['idno']?>
     <?php }} ?>
-      <input class="form-control" id="employee_fname" type="text" name="employee_fname" value="<?php echo $fname; ?>">
-      <input class="form-control" id="employee_lname" type="text" name="employee_lname" value="<?php echo $lname; ?>">
-      <input class="form-control" id="employee_idno" type="text" name="employee_idno" value="<?php echo $employeeID; ?>">
+      <input class="form-control" id="employee_fname" type="hidden" name="employee_fname" value="<?php echo $fname; ?>">
+      <input class="form-control" id="employee_lname" type="hidden" name="employee_lname" value="<?php echo $lname; ?>">
+      <input class="form-control" id="employee_idno" type="hidden" name="employee_idno" value="<?php echo $employeeID; ?>">
     <div class="form-group pt-3 mx-auto" style="width: 95%;">
       <label for="title" style="font-size: 14px;">Title <span class="text-muted" style="font-size: 10px;">e.g. "Complete Assignment"</span></label>
       <input class="form-control" id="title" type="text" name="title" value="" required>
