06872caaf584bfe78e0e4b7d781594ff57eac5ac
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-19 09:45:14 -0600

    updates

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -10,13 +10,13 @@
                 <i class="bi bi-info-circle"></i>
                 <span>  Information</span>
             </a>
-            <a href="<?php echo BASE_URL . '/pages/info.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
-                <i class="bi bi-list-task"></i>
-                <span>  Tasks</span>
-            </a>
             <a href="<?php echo BASE_URL . '/pages/timesheet.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
                 <i class="bi bi-clock"></i>
                 <span>  Timesheet</span>
+            </a>
+            <a href="<?php echo BASE_URL . '/pages/info.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
+                <i class="bi bi-list-task"></i>
+                <span>  Tasks</span>
             </a>
             <?php } ?>
 
