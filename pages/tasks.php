567cebf7e1eabc69985d6f243b18a622a30d31b7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-19 10:54:31 -0600

    update

--- pages/tasks.php
+++ pages/tasks.php
@@ -194,7 +194,7 @@ if(!isLoggedIn()){
             
             <form method="post" action="">
               <input type="hidden" name="jobID" value="<?php echo $jobID; ?>" />
-              <button  style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit"><span class="badge text-bg-success">Edit</span></button>
+              <button  style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit"><span class="badge text-bg-success editbtn">Edit</span></button>
               <button onclick="return confirm('Be Careful, Can\'t be undone! \r\nOK to delete?')" style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="terminated"><span class="badge text-bg-danger">Delete</span></button>
             </form>
           </td>
@@ -214,7 +214,82 @@ if(!isLoggedIn()){
   </div> 
 <!-- END MAIN -->
 
+<!-- MODAL -->
+    <!-- EDIT POP UP FORM (Bootstrap MODAL) -->
+    <div class="modal fade" id="editmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"> Edit Student Data </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="updatecode.php" method="POST">
+
+                    <div class="modal-body">
+
+                        <input type="hidden" name="update_id" id="update_id">
+
+                        <div class="form-group">
+                            <label> First Name </label>
+                            <input type="text" name="fname" id="fname" class="form-control"
+                                placeholder="Enter First Name">
+                        </div>
+
+                        <div class="form-group">
+                            <label> Last Name </label>
+                            <input type="text" name="lname" id="lname" class="form-control"
+                                placeholder="Enter Last Name">
+                        </div>
+
+                        <div class="form-group">
+                            <label> Course </label>
+                            <input type="text" name="course" id="course" class="form-control"
+                                placeholder="Enter Course">
+                        </div>
+
+                        <div class="form-group">
+                            <label> Phone Number </label>
+                            <input type="text" name="contact" id="contact" class="form-control"
+                                placeholder="Enter Phone Number">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" name="updatedata" class="btn btn-primary">Update Data</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+<!-- END MODAL -->
+
 <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+
+<script>
+    $(document).ready(function () {
+
+        $('.editbtn').on('click', function () {
+            $('#editmodal').modal('show');
+            $tr = $(this).closest('tr');
+            var data = $tr.children("td").map(function () {
+                return $(this).text();
+            }).get();
+
+            console.log(data);
+
+            $('#update_id').val(data[0]);
+            $('#fname').val(data[1]);
+            $('#lname').val(data[2]);
+            $('#course').val(data[3]);
+            $('#contact').val(data[4]);
+        });
+    });
+</script>
 
 </body>
 </html>
