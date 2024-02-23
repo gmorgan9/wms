692a9f1090a828b7eb4901e7589c4cae95e1294d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-23 11:09:28 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -194,5 +194,6 @@ session_start();
     <script src="../assets/js/auditGraph.js?v=<?php echo time(); ?>"></script>
     <script src="../assets/js/grab_tasks.js?v=<?php echo time(); ?>"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
