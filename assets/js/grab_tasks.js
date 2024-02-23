bd58e885e0534fe3726b3cf68ed78484b1648058
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-23 11:06:27 -0600

    update

--- assets/js/grab_tasks.js
+++ assets/js/grab_tasks.js
@@ -8,39 +8,6 @@ document.addEventListener("DOMContentLoaded", function() {
         return date.toLocaleDateString('en-GB', options);
     }
 
-    // Function to create and show the popup menu
-    function showPopupMenu(event, taskId) {
-        // Create a popup menu
-        const popupMenu = document.createElement("div");
-        popupMenu.classList.add("popup-menu");
-        popupMenu.innerHTML = `
-            <ul>
-                <li onclick="viewTask(${taskId})">View</li>
-                <li onclick="editTask(${taskId})">Edit</li>
-                <li onclick="deleteTask(${taskId})">Delete</li>
-            </ul>
-        `;
-
-        // Position the popup menu relative to the click event
-        popupMenu.style.top = `${event.clientY}px`;
-        popupMenu.style.left = `${event.clientX}px`;
-
-        // Append the popup menu to the document body
-        document.body.appendChild(popupMenu);
-
-        // Close the popup menu when clicking outside of it
-        document.addEventListener("click", closePopupMenu);
-    }
-
-    // Function to close the popup menu
-    function closePopupMenu(event) {
-        const popupMenu = document.querySelector(".popup-menu");
-        if (popupMenu && !popupMenu.contains(event.target)) {
-            popupMenu.remove();
-            document.removeEventListener("click", closePopupMenu);
-        }
-    }
-
     // Function to fetch tasks from PHP file
     function fetchTasks() {
         axios.get("../app/functions/latestTasks.php")
@@ -63,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         </div>
                         <p class="text-secondary my-auto" style="margin-left: 80px;">${task.client_name}</p>
                         <p class="text-secondary my-auto end">
-                            <i class="bi bi-three-dots-vertical" onclick="showPopupMenu(event, ${task.id})"></i>
+                            <a href="#" onclick="showPopupMenu(event, ${task.id})"><i class="bi bi-three-dots-vertical"></i></a>
                         </p>
                     `;
                     taskContainer.appendChild(taskCard);
