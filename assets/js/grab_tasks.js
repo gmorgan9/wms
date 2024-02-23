fa7be37baf8ceeb4ffee3ec864bd030c5d360576
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-23 11:07:11 -0600

    update

--- assets/js/grab_tasks.js
+++ assets/js/grab_tasks.js
@@ -8,6 +8,40 @@ document.addEventListener("DOMContentLoaded", function() {
         return date.toLocaleDateString('en-GB', options);
     }
 
+    // Function to create and show the popup menu
+    function showPopupMenu(event, taskId) {
+        event.preventDefault();
+
+        const popupMenu = document.createElement("div");
+        popupMenu.classList.add("popup-menu");
+        popupMenu.innerHTML = `
+            <ul>
+                <li onclick="viewTask(${taskId})">View</li>
+                <li onclick="editTask(${taskId})">Edit</li>
+                <li onclick="deleteTask(${taskId})">Delete</li>
+            </ul>
+        `;
+
+        // Position the popup menu relative to the click event
+        popupMenu.style.top = `${event.clientY}px`;
+        popupMenu.style.left = `${event.clientX}px`;
+
+        // Append the popup menu to the document body
+        document.body.appendChild(popupMenu);
+
+        // Close the popup menu when clicking outside of it
+        document.addEventListener("click", closePopupMenu);
+    }
+
+    // Function to close the popup menu
+    function closePopupMenu(event) {
+        const popupMenu = document.querySelector(".popup-menu");
+        if (popupMenu && !popupMenu.contains(event.target)) {
+            popupMenu.remove();
+            document.removeEventListener("click", closePopupMenu);
+        }
+    }
+
     // Function to fetch tasks from PHP file
     function fetchTasks() {
         axios.get("../app/functions/latestTasks.php")
